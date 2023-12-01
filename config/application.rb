1eba4c9404f41fd91681ff2e72e74247a8fa7461
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-01 16:53:12 +0100

    Configure locales

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module DataPass
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.time_zone = 'Europe/Paris'
+    config.i18n.available_locales = [:fr]
+    config.i18n.default_locale = :fr
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
