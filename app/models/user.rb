b46c46d08e0b28bf2b1bbab7663344db90205e6a
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-02 19:55:16 +0100

    Introduce User#full_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   validates :external_id, presence: true, uniqueness: true
 
   has_and_belongs_to_many :organizations
+
+  def full_name
+    "#{family_name} #{given_name}"
+  end
 end
