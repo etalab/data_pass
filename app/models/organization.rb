108eaf45e579ee90139f9c32cd3aab1a9ce15e3d
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-04 22:56:50 +0100

    Introduce Organization#raison_sociale

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,8 @@ class Organization < ApplicationRecord
   validates :siret, presence: true, uniqueness: true, siret: true
 
   has_and_belongs_to_many :users
+
+  def raison_sociale
+    mon_compte_pro_payload['label']
+  end
 end
