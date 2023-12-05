def5a3330a9e87425479174a5a3e749e86de8eec
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-05 17:06:00 +0100

    Add flash methods and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   helper ActiveLinks
+
+  def error_message_for(object, title:, id: nil)
+    flash_message(:error, title:, description: object.errors.full_messages, id:, activemodel: true)
+  end
+
+  def error_message(title:, description: nil, id: nil)
+    flash_message(:error, title:, description:, id:)
+  end
+
+  def success_message(title:, description: nil, id: nil)
+    flash_message(:success, title:, description:, id:)
+  end
+
+  def info_message(title:, description: nil, id: nil)
+    flash_message(:info, title:, description:, id:)
+  end
+
+  private
+
+  def flash_message(kind, title:, description:, id:, activemodel: false)
+    flash[kind] ||= {}
+    flash[kind]['title'] = title
+    flash[kind]['description'] = description
+    flash[kind]['id'] = id
+    flash[kind]['activemodel'] = activemodel
+  end
 end
