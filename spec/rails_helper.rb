e8e919c3e4f05a1d931e6491fe44bdeabab031a5
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-05 07:51:35 +0100

    Add more helpers for features

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,6 +43,7 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include SessionsHelpers, type: :feature
+  config.include FeaturesHelpers, type: :feature
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
---
+++ spec/support/feature_helpers.rb
@@ -0,0 +1,11 @@
+module FeaturesHelpers
+  include ActionView::RecordIdentifier
+
+  def css_id(record)
+    "##{dom_id(record)}"
+  end
+
+  def input_identifier(klass, attribute)
+    "#{klass.model_name.singular}_#{attribute}"
+  end
+end
