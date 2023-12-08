6b0141b96212fc517a848653372e38652b4035e8
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-08 15:11:35 +0100

    Enhance factory

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     last_mon_compte_pro_updated_at { DateTime.now }
 
     after(:build) do |organization|
-      organization.mon_compte_pro_payload = build(:mon_compte_pro_payload, siret: organization.siret) if organization.mon_compte_pro_payload.blank?
+      organization.mon_compte_pro_payload = build(:organization_hash_from_mon_compte_pro, siret: organization.siret) if organization.mon_compte_pro_payload.blank?
     end
   end
 end
