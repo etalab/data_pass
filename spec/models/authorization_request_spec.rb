ba4a926cb31a2deddb9088cb75d1b8e00a5afce9
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-12 17:56:25 +0100

    Introduce API Entreprise
    Prepare to use a multi steps form in the following commit.
    Use need_complete_validation? with steps, in order to make validation
    step by step too.
    
    Related https://github.com/zombocom/wicked/wiki/Building-Partial-Objects-Step-by-Step#the-solution

--- spec/models/authorization_request_spec.rb
+++ spec/models/authorization_request_spec.rb
@@ -1,6 +1,11 @@
 RSpec.describe AuthorizationRequest do
   it 'has valid factories' do
-    expect(build(:authorization_request)).to be_valid
-    expect(build(:authorization_request, state: 'submitted')).to be_valid
+    %w[
+      hubee_cert_dc
+      api_entreprise
+    ].each do |kind|
+      expect(build(:authorization_request, kind)).to be_valid
+      expect(build(:authorization_request, kind, state: 'submitted')).to be_valid
+    end
   end
 end
