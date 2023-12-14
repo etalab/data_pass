88adfb234e6465d3d5b6f7d3d992b80aeda1e791
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-14 16:14:30 +0100

    Harden validation on Organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,9 @@
 class Organization < ApplicationRecord
   validates :siret, presence: true, uniqueness: true, siret: true
 
+  validates :mon_compte_pro_payload, presence: true
+  validates :last_mon_compte_pro_updated_at, presence: true
+
   has_and_belongs_to_many :users
 
   has_many :authorization_requests,
