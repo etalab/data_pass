54205717448addcaa0519b714362c8e3da066fb5
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-19 11:37:27 +0100

    Install and configure pagination with DSFR

--- app/controllers/instruction/authorization_requests_controller.rb
+++ app/controllers/instruction/authorization_requests_controller.rb
@@ -1,5 +1,5 @@
 class Instruction::AuthorizationRequestsController < InstructionController
   def index
-    @authorization_requests = policy_scope(AuthorizationRequest, policy_scope_class: Instruction::AuthorizationRequestPolicy::Scope).all
+    @authorization_requests = policy_scope(AuthorizationRequest, policy_scope_class: Instruction::AuthorizationRequestPolicy::Scope).page(params[:page])
   end
 end
