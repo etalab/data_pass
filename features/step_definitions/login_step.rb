37582d2ce4e624865678070f90bfe071088ea238
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-20 11:28:29 +0100

    Refactor login steps for cucumber

--- features/step_definitions/login_step.rb
+++ features/step_definitions/login_step.rb
@@ -1,10 +1,8 @@
-Sachantque('je suis un demandeur') do
-  @current_user_email = 'demandeur@gouv.fr'
-
+def mock_mon_compte_pro(user)
   OmniAuth.config.mock_auth[:mon_compte_pro] = OmniAuth::AuthHash.new({
     provider: :mon_compte_pro,
-    uid: '1',
-    info: attributes_for(:mon_compte_pro_payload, email: @current_user_email),
+    uid: user.external_id,
+    info: attributes_for(:mon_compte_pro_payload, email: user.email, external_id: user.external_id, siret: user.current_organization.siret),
     credentials: {
       token: 'token',
       expires_at: 1.hour.from_now.to_i,
@@ -13,24 +11,21 @@ Sachantque('je suis un demandeur') do
   })
 end
 
+Sachantque('je suis un demandeur') do
+  @current_user_email = 'demandeur@gouv.fr'
+  user = User.find_by(email: @current_user_email) || FactoryBot.create(:user, email: @current_user_email)
+
+  mock_mon_compte_pro(user)
+end
+
 Sachantque('je suis un instructeur {string}') do |kind|
   @current_user_email = "#{kind.parameterize}@gouv.fr"
   user = User.find_by(email: @current_user_email) || FactoryBot.create(:user, email: @current_user_email)
+  mock_mon_compte_pro(user)
 
-  user.roles << "#{find_form_from_name(kind).authorization_request_class.to_s.underscore.split('/').last}:instructor"
+  user.roles << "#{find_factory_trait_from_name(kind)}:instructor"
   user.roles.uniq!
   user.save!
-
-  OmniAuth.config.mock_auth[:mon_compte_pro] = OmniAuth::AuthHash.new({
-    provider: :mon_compte_pro,
-    uid: user.external_id,
-    info: attributes_for(:mon_compte_pro_payload, email: @current_user_email, external_id: user.external_id),
-    credentials: {
-      token: 'token',
-      expires_at: 1.hour.from_now.to_i,
-      expires: true
-    }
-  })
 end
 
 Sachantque('je me connecte') do
