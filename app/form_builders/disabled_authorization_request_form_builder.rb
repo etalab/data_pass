163cf3117ccdaa6e1dd8427afc0b931227bf3f8c
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-24 15:03:36 +0100

    Introduce dsfr_select for form builder

--- app/form_builders/disabled_authorization_request_form_builder.rb
+++ app/form_builders/disabled_authorization_request_form_builder.rb
@@ -2,4 +2,8 @@ class DisabledAuthorizationRequestFormBuilder < AuthorizationRequestFormBuilder
   def readonly?
     true
   end
+
+  def dsfr_select_tag(attribute, _choices, opts)
+    text_field(attribute, class: input_classes(opts), autocomplete: 'off', **enhance_input_options(opts).except(:class))
+  end
 end
