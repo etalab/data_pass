245a63a390aa8b9e342430e25b6153e4a7d52497
Loïc Delmaire <loic.delmaire@gmail.com>
2023-12-24 12:00:14 +0100

    Enhance development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,10 +76,10 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
