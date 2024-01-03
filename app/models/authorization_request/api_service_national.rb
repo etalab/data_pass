0b41f741c4e20964ac51a663b1b44a2c9324f26f
Isabelle Lafont <isalafont@gmail.com>
2024-01-03 11:48:52 +0100

    Model api service national after rebase

---
+++ app/models/authorization_request/api_service_national.rb
@@ -0,0 +1,17 @@
+class AuthorizationRequest::APIServiceNational < AuthorizationRequest
+  include AuthorizationExtensions::BasicInfos
+  include AuthorizationExtensions::PersonalData
+  include AuthorizationExtensions::CadreJuridique
+  include AuthorizationExtensions::GDPRContacts
+
+  add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
+
+  add_attributes :date_prevue_mise_en_production,
+    :volumetrie_approximative
+
+  %i[
+    responsable_technique
+  ].each do |contact_kind|
+    contact contact_kind, validation_condition: -> { need_complete_validation?(:contacts) }
+  end
+end
