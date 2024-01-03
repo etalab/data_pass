b23e61687bca070690332d9acff32db0bf0c4e61
Isabelle Lafont <isalafont@gmail.com>
2024-01-03 16:19:38 +0100

    Test formulaire API Service National

--- app/models/authorization_request/api_service_national.rb
+++ app/models/authorization_request/api_service_national.rb
@@ -9,6 +9,10 @@ class AuthorizationRequest::APIServiceNational < AuthorizationRequest
   add_attributes :date_prevue_mise_en_production,
     :volumetrie_approximative
 
+  add_scopes(validation: {
+    presence: true, if: -> { need_complete_validation?(:scopes) }
+  })
+
   %i[
     responsable_technique
   ].each do |contact_kind|
