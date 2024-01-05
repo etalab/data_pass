e181b135b49bdd1f89bbad1e8a4536a95708e132
Loïc Delmaire <loic.delmaire@gmail.com>
2024-01-05 21:51:41 +0100

    Introduce AuthorizationRequestMailer#refused

--- spec/mailers/authorization_request_mailer_spec.rb
+++ spec/mailers/authorization_request_mailer_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe AuthorizationRequestMailer do
       expect(mail.body.encoded).to match('a été validée')
     end
   end
+
+  describe '#refused' do
+    subject(:mail) do
+      described_class.with(
+        authorization_request:
+      ).refused
+    end
+
+    let(:authorization_request) { create(:authorization_request, :api_entreprise, :refused) }
+
+    it 'sends the email to the applicant' do
+      expect(mail.to).to eq([authorization_request.applicant.email])
+    end
+
+    it 'renders valid template, with denial reason' do
+      expect(mail.body.encoded).to match('a été refusée')
+      expect(mail.body.encoded).to match(authorization_request.denial.reason)
+    end
+  end
 end
--- spec/mailers/previews/authorization_request_preview.rb
+++ spec/mailers/previews/authorization_request_preview.rb
@@ -2,4 +2,8 @@ class AuthorizationRequestPreview < ActionMailer::Preview
   def validated
     AuthorizationRequestMailer.with(authorization_request: AuthorizationRequest.where(state: 'validated').first).validated
   end
+
+  def refused
+    AuthorizationRequestMailer.with(authorization_request: AuthorizationRequest.where(state: 'refused').first).refused
+  end
 end
