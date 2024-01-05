51c1b622e80dabea651322cd03cd00c6c2918f29
Loïc Delmaire <loic.delmaire@gmail.com>
2024-01-05 17:21:34 +0100

    Configure mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,20 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'DataPass <notifications@api.gouv.fr>'
+
   layout 'mailer'
+
+  before_action :extract_host
+
+  def extract_host
+    default_url_options[:host] = current_host
+  end
+
+  def current_host
+    case Rails.env
+    when 'production'
+      Rails.application.host
+    else
+      'http://localhost:3000'
+    end
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.host = ENV.fetch("HOST", "localhost:3000")
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
