6c5854d2a50344ccbe5841ffe173a595ae937377
Loïc Delmaire <loic.delmaire@gmail.com>
2024-01-10 21:04:28 +0100

    Follow redirects on success within turbo frame
    Ref https://stackoverflow.com/a/75704489

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails'
 import 'controllers'
+
+document.addEventListener('turbo:frame-missing', (event) => {
+  const { detail: { response, visit } } = event
+  event.preventDefault()
+  visit(response)
+})
