01e6c777b3cd170e43d3e43b6f7a92759a11fc05
Loïc Delmaire <loic.delmaire@gmail.com>
2024-01-25 15:03:23 +0100

    Scout commit

--- spec/models/authorization_request_form_spec.rb
+++ spec/models/authorization_request_form_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe AuthorizationRequestForm do
   describe '.all' do
-    it 'returns a list of all authorizations' do
+    it 'returns a list of all authorizations forms' do
       expect(described_class.all).to be_all { |a| a.is_a? AuthorizationRequestForm }
     end
   end
