2bc7bb5e541b96afb67bc5b0ca93de510412b4fb
Loïc Delmaire <loic.delmaire@gmail.com>
2024-02-01 18:26:43 +0100

    Add more tests for SubmitAuthorizationRequest

--- spec/organizers/submit_authorization_request_spec.rb
+++ spec/organizers/submit_authorization_request_spec.rb
@@ -1,23 +1,65 @@
 RSpec.describe SubmitAuthorizationRequest do
   describe '.call' do
-    subject(:submit_authorization_request) { described_class.call(user: authorization_request.applicant, authorization_request:) }
+    subject(:submit_authorization_request) { described_class.call(user: authorization_request.applicant, authorization_request:, authorization_request_params:) }
+
+    let(:authorization_request_params) { nil }
 
     context 'with authorization request in draft state' do
-      let(:authorization_request) { create(:authorization_request, :api_entreprise, :draft, fill_all_attributes: true) }
+      context 'with valid authorization request' do
+        let(:authorization_request) { create(:authorization_request, :api_entreprise, :draft, fill_all_attributes: true) }
 
-      it { is_expected.to be_success }
+        it { is_expected.to be_success }
 
-      it 'changes state to submitted' do
-        expect { submit_authorization_request }.to change { authorization_request.reload.state }.from('draft').to('submitted')
+        it 'changes state to submitted' do
+          expect { submit_authorization_request }.to change { authorization_request.reload.state }.from('draft').to('submitted')
+        end
+
+        include_examples 'creates an event', event_name: :submit
       end
 
-      include_examples 'creates an event', event_name: :submit
+      context 'with invalid params' do
+        let(:authorization_request) { create(:authorization_request, :api_entreprise, :draft, fill_all_attributes: true) }
+
+        let(:authorization_request_params) { ActionController::Parameters.new(intitule: '') }
+
+        it { is_expected.to be_failure }
+
+        it 'does not change state' do
+          expect { submit_authorization_request }.not_to change { authorization_request.reload.state }
+        end
+
+        it 'does not create an event' do
+          expect { submit_authorization_request }.not_to change { authorization_request.events.count }
+        end
+
+        it 'does not updates params' do
+          expect { submit_authorization_request }.not_to change { authorization_request.reload.intitule }
+        end
+      end
+
+      context 'with invalid authorization request' do
+        let(:authorization_request) { create(:authorization_request, :api_entreprise, :draft) }
+
+        it { is_expected.to be_failure }
+
+        it 'does not change state' do
+          expect { submit_authorization_request }.not_to change { authorization_request.reload.state }
+        end
+
+        it 'does not create an event' do
+          expect { submit_authorization_request }.not_to change { authorization_request.events.count }
+        end
+      end
     end
 
     context 'with authorization request in validated state' do
       let(:authorization_request) { create(:authorization_request, :api_entreprise, :validated) }
 
       it { is_expected.to be_a_failure }
+
+      it 'does not create an event' do
+        expect { submit_authorization_request }.not_to change { authorization_request.events.count }
+      end
     end
   end
 end
