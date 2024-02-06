aebe9e8e523e7734170f7fe69c2e294108ba3322
Loïc Delmaire <loic.delmaire@gmail.com>
2024-02-06 10:48:08 +0100

    Enhance factory testing

--- spec/models/authorization_request_spec.rb
+++ spec/models/authorization_request_spec.rb
@@ -1,16 +1,10 @@
 RSpec.describe AuthorizationRequest do
-  def all_form_uids
-    AuthorizationRequestForm.all.map { |form| form.uid.underscore }
-  end
-
-  def state_machine_state_names
-    AuthorizationRequest.state_machine.states.map(&:name)
-  end
-
-  describe 'Authorization Request has valid factories' do
+  describe 'factories' do
     it 'has valid factories for all states and all form_uids' do
-      all_form_uids.each do |form_uid|
-        state_machine_state_names.each { |state_name| expect(build(:authorization_request, form_uid.to_sym, state_name)).to be_valid }
+      AuthorizationRequestForm.all.each do |form|
+        AuthorizationRequest.state_machine.states.map(&:name).each do |state_name|
+          expect(build(:authorization_request, form.uid.underscore, state_name)).to be_valid
+        end
       end
     end
 
