596d5bd22ac42117366e334aed245468636dc5da
Loïc Delmaire <loic.delmaire@gmail.com>
2024-02-24 10:05:21 +0100

    Introduce changelog display within authorization request history

--- app/decorators/authorization_request_event_decorator.rb
+++ app/decorators/authorization_request_event_decorator.rb
@@ -7,7 +7,48 @@ class AuthorizationRequestEventDecorator < ApplicationDecorator
     user.full_name
   end
 
+  def name
+    case object.name
+    when 'submit'
+      if initial_submit?
+        'initial_submit'
+      else
+        'submit'
+      end
+    else
+      object.name
+    end
+  end
+
   def text
-    entity.try(:reason)
+    case name
+    when 'refuse', 'request_changes'
+      entity.reason
+    when 'submit'
+      humanized_changelog
+    end
+  end
+
+  private
+
+  def humanized_changelog
+    h.content_tag(:ul) do
+      object.entity.diff.map { |attribute, values|
+        h.content_tag(:li, build_attribute_change(attribute, values))
+      }.join.html_safe
+    end
+  end
+
+  def build_attribute_change(attribute, values)
+    t(
+      'authorization_request_event.changelog_entry',
+      attribute: object.authorization_request.class.human_attribute_name(attribute),
+      old_value: values.first,
+      new_value: values.last,
+    )
+  end
+
+  def initial_submit?
+    object.entity.diff.all? { |_attribute, values| values.first.nil? }
   end
 end
