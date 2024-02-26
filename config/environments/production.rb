f68646f15cddeeba673c29584d2d0cea8a8edbeb
Loïc Delmaire <loic.delmaire@gmail.com>
2024-02-26 19:16:54 +0100

    Install good_job

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter = :resque
+  config.active_job.queue_adapter = :good_job
   # config.active_job.queue_name_prefix = "data_pass_production"
 
   config.action_mailer.delivery_method = :mailjet
