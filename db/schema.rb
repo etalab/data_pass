1eb453fede2e927b6f8035498c2e282a3da7d0b5
Loïc Delmaire <loic.delmaire@gmail.com>
2024-02-26 16:45:00 +0100

    Clean db schema
    There is no versions table, forgot to clean the database

--- db/schema.rb
+++ db/schema.rb
@@ -140,17 +140,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_23_235741) do
     t.index ["external_id"], name: "index_users_on_external_id", unique: true, where: "(external_id IS NOT NULL)"
   end
 
-  create_table "versions", force: :cascade do |t|
-    t.string "item_type", null: false
-    t.bigint "item_id", null: false
-    t.string "event", null: false
-    t.string "whodunnit"
-    t.jsonb "object"
-    t.jsonb "object_changes"
-    t.datetime "created_at"
-    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "authorization_request_changelogs", "authorization_requests"
