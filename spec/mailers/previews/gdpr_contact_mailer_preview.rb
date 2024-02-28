054d466dd4d5acca2e652d19a87093dc8f845e21
Mehdi Farsi <mehdifarsi.pro@gmail.com>
2024-02-28 16:29:06 +0100

    add GDPRContactMailer preview

---
+++ spec/mailers/previews/gdpr_contact_mailer_preview.rb
@@ -0,0 +1,19 @@
+class GDPRContactMailerPreview < ActionMailer::Preview
+  AuthorizationExtensions::GDPRContacts::GDPR_CONTACTS.each do |contact|
+    define_method contact do
+      GDPRContactMailer.with(params_for(contact)).send(contact)
+    end
+  end
+
+  private
+
+  def params_for(contact)
+    {
+      authorization_request: find_authorization_request_by(contact)
+    }
+  end
+
+  def find_authorization_request_by(contact)
+    AuthorizationRequest.where.not("data ? '#{contact}_email'").first
+  end
+end
