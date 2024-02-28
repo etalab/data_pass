1b2b75358f25bf9bd20360556ea087d54426beb0
Isabelle Lafont <isalafont@gmail.com>
2024-02-28 12:14:30 +0100

    Handle when current_organization is nul to display home page for hubee

--- app/policies/authorization_request_form_policy.rb
+++ app/policies/authorization_request_form_policy.rb
@@ -13,6 +13,6 @@ class AuthorizationRequestFormPolicy < ApplicationPolicy
   end
 
   def another_authorization_request_with_same_type_exists?
-    current_organization.authorization_requests.where(type: record.authorization_request_class.to_s).any?
+    current_organization && current_organization.authorization_requests.where(type: record.authorization_request_class.to_s).any?
   end
 end
