3c129258c40b6751ecf669a7ab7a7891bd7c1424
Loïc Delmaire <loic.delmaire@gmail.com>
2024-02-29 17:33:44 +0100

    Enhance sentry configuration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,6 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_dsn
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = [:sentry_logger, :active_support_logger, :http_logger]
 
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
-  # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
 end
