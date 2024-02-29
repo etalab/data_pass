dc627c7e9291496ffd18df8021d356361fbc12ec
Loïc Delmaire <loic.delmaire@gmail.com>
2024-02-29 17:34:39 +0100

    Enable sandbox environment for sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_dsn
   config.breadcrumbs_logger = [:sentry_logger, :active_support_logger, :http_logger]
+  config.enabled_environments = %w[sandbox production]
 
   config.traces_sample_rate = 1.0
 end
