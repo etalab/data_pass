744fadd7e44075d9dc62a3a3089a61e98c1a7a17
Mehdi Farsi <mehdifarsi.pro@gmail.com>
2024-03-01 12:05:14 +0100

    refactor GDPRContactsMailer specs

--- spec/interactors/deliver_gdpr_contacts_mails_spec.rb
+++ spec/interactors/deliver_gdpr_contacts_mails_spec.rb
@@ -5,26 +5,19 @@ RSpec.describe DeliverGDPRContactsMails do
     context 'when no GDPR contact' do
       let(:authorization_request) { create(:authorization_request) }
 
-      it 'does not deliver any emails to RGPD contacts' do
-        expect(GDPRContactMailer).not_to receive(:with)
-
-        expect(subject).to be_truthy
+      AuthorizationExtensions::GDPRContacts::NAMES.each do |gdpr_contact|
+        it "does not deliver a notification email to #{gdpr_contact}" do
+          expect { subject }.not_to have_enqueued_mail(GDPRContactMailer, gdpr_contact)
+        end
       end
     end
 
     context 'when all GDPR contacts' do
       let(:authorization_request) { create(:authorization_request, :api_particulier, fill_all_attributes: true) }
 
-      it 'delivers emails to RGPD contacts' do
-        AuthorizationExtensions::GDPRContacts::NAMES.each do |contact|
-          contact_mailer = instance_double(ActionMailer::MessageDelivery)
-
-          allow(GDPRContactMailer).to receive_message_chain(:with, contact).and_return(contact_mailer) # rubocop:disable RSpec/MessageChain
-
-          expect(contact_mailer).to receive(:deliver_later)
-        end
-
-        expect(subject).to be_truthy
+      it 'delivers notification emails to RGPD contacts' do
+        expect { subject }.to have_enqueued_mail(GDPRContactMailer, :responsable_traitement)
+          .and have_enqueued_mail(GDPRContactMailer, :delegue_protection_donnees)
       end
     end
   end
