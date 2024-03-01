8e62bafd6aff0b667a4661b283906c1f56d64e60
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-01 15:47:05 +0100

    Add insee payload if exists in organization factory

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -5,6 +5,11 @@ FactoryBot.define do
 
     after(:build) do |organization|
       organization.mon_compte_pro_payload = build(:organization_hash_from_mon_compte_pro, siret: organization.siret) if organization.mon_compte_pro_payload.blank?
+
+      if Rails.root.join('spec', 'fixtures', 'insee', "#{organization.siret}.json").exist?
+        organization.insee_payload = JSON.parse(Rails.root.join('spec', 'fixtures', 'insee', "#{organization.siret}.json").read)
+        organization.last_insee_payload_updated_at = DateTime.now
+      end
     end
   end
 end
