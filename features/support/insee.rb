8964b8c8f9d99818ddfec2576c863f3435dd14e5
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-01 18:48:15 +0100

    Global stub for cucumber feature on INSEE calls
    No need to have real call, features with specific tests on INSEE data
    will be build thanks to FactoryBot.

---
+++ features/support/insee.rb
@@ -0,0 +1,14 @@
+Before do |_scenario|
+  stub_request(:post, 'https://api.insee.fr/token').to_return(
+    status: 200,
+    body: {
+      access_token: 'token',
+    }.to_json,
+  )
+
+  stub_request(:get, %r{^https://api.insee.fr/entreprises/sirene/V3/siret/}).to_return(
+    status: 200,
+    headers: { 'Content-Type' => 'application/json' },
+    body: Rails.root.join('spec/fixtures/insee/13002526500013.json').read
+  )
+end
