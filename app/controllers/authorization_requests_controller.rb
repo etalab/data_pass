8ea66ed52f23ca71624d5903a773a7e1e28d8cfa
Isabelle Lafont <isalafont@gmail.com>
2024-03-05 14:45:39 +0100

    Fix redirection to previous url once connected
      - Refacto method show cause rubocop

--- app/controllers/authorization_requests_controller.rb
+++ app/controllers/authorization_requests_controller.rb
@@ -14,18 +14,23 @@ class AuthorizationRequestsController < AuthenticatedUserController
   def show
     @authorization_request = AuthorizationRequest.find(params[:id])
 
-    if user_signed_in?
-      authorize @authorization_request
-      redirect_to authorization_request_form_path(form_uid: @authorization_request.form_uid, id: @authorization_request.id)
-    else
-      @authorization_definition = @authorization_request.definition
-      render 'pages/home'
-    end
+    handle_authorization
   rescue Pundit::NotAuthorizedError
     redirect_to summary_authorization_request_form_path(form_uid: @authorization_request.form.uid, id: @authorization_request.id)
   end
 
   private
+
+  def handle_authorization
+    if user_signed_in?
+      authorize @authorization_request
+      redirect_to authorization_request_form_path(form_uid: @authorization_request.form_uid, id: @authorization_request.id)
+    else
+      @authorization_definition = @authorization_request.definition
+      save_redirect_path
+      render 'pages/home'
+    end
+  end
 
   def id_sanitized
     params[:id].underscore
