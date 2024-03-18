34c6b1c9fcf465e9a32326529b40742979656314
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-18 11:48:44 +0000

    Add host within authorization context
    Prepare to retrieve data based on subdomain in order to restrict access
    
    Ref https://github.com/varvet/pundit?tab=readme-ov-file#additional-context

--- app/controllers/authenticated_user_controller.rb
+++ app/controllers/authenticated_user_controller.rb
@@ -4,6 +4,10 @@ class AuthenticatedUserController < ApplicationController
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def pundit_user
+    UserContext.new(current_user, request.host)
+  end
+
   def user_not_authorized
     flash[:error] = {
       title: t('application.user_not_authorized.title')
