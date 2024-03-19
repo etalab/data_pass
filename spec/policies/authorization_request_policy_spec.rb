eb5a4666df212173d4f15a0880852ebdfc608034
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-03-19 11:08:23 +0100

    Add Rspec

---
+++ spec/policies/authorization_request_policy_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe AuthorizationRequestPolicy do
+  describe '#new?' do
+    subject { described_class.new(user, authorization_request_class).new? }
+
+    let(:user) { create(:user) }
+
+    describe 'HubEE' do
+      let(:definition) { AuthorizationDefinition.find('hubee_cert_dc') }
+      let(:authorization_request_class) { definition.authorization_request_class }
+
+      context 'when there already is an authorization_request is archived' do
+        before { create(:authorization_request, :hubee_cert_dc, :archived, applicant: user) }
+
+        it { is_expected.to be_truthy }
+      end
+
+      context 'when there already is another authorization_request not archived' do
+        before { create(:authorization_request, :hubee_cert_dc, applicant: user) }
+
+        it { is_expected.to be_falsey }
+      end
+    end
+  end
+end
