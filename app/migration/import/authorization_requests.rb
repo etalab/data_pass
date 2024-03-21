9c6d77d001cafa50f6e5301662226963b9731967
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-21 13:40:54 +0000

    Migration: do not import old unused data

--- app/migration/import/authorization_requests.rb
+++ app/migration/import/authorization_requests.rb
@@ -146,13 +146,13 @@ class Import::AuthorizationRequests < Import::Base
   end
 
   def import?(enrollment_row)
-    !old_draft?(enrollment_row) &&
+    !old_unused?(enrollment_row) &&
       !ignore?(enrollment_row['id']) &&
       from_target_api_to_type(enrollment_row).present?
   end
 
-  def old_draft?(enrollment_row)
-    enrollment_row['status'] == 'draft' &&
+  def old_unused?(enrollment_row)
+    %w[refused changes_requested draft archived].include?(enrollment_row['status'])
       DateTime.parse(enrollment_row['created_at']) < DateTime.new(2022, 1, 1)
   end
 
