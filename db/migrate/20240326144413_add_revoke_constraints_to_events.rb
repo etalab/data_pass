f08143212731bafbf5b8efd9c88f313e9ca5b5fb
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-03-26 15:46:20 +0100

    Add migration revoke constraints

---
+++ db/migrate/20240326144413_add_revoke_constraints_to_events.rb
@@ -0,0 +1,45 @@
+class AddRevokeConstraintsToEvents < ActiveRecord::Migration[7.1]
+  def up
+    execute <<-SQL
+      ALTER TABLE authorization_request_events
+      DROP CONSTRAINT entity_type_validation
+    SQL
+
+    execute <<-SQL
+      ALTER TABLE authorization_request_events
+      ADD CONSTRAINT entity_type_validation
+      CHECK (
+        (name = 'refuse' AND entity_type = 'DenialOfAuthorization') OR
+        (name = 'request_changes' AND entity_type = 'InstructorModificationRequest') OR
+        (name = 'approve' AND entity_type = 'Authorization') OR
+        (name = 'reopen' AND entity_type = 'Authorization') OR
+        (name = 'submit' AND entity_type = 'AuthorizationRequestChangelog') OR
+        (name = 'applicant_message' AND entity_type = 'Message') OR
+        (name = 'instructor_message' AND entity_type = 'Message') OR
+        (name = 'revoke' AND entity_type = 'DenialOfAuthorization') OR
+        (entity_type = 'AuthorizationRequest')
+      )
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      ALTER TABLE authorization_request_events
+      DROP CONSTRAINT entity_type_validation
+    SQL
+
+    execute <<-SQL
+      ALTER TABLE authorization_request_events
+      ADD CONSTRAINT entity_type_validation
+      CHECK (
+        (name = 'refuse' AND entity_type = 'DenialOfAuthorization') OR
+        (name = 'request_changes' AND entity_type = 'InstructorModificationRequest') OR
+        (name = 'approve' AND entity_type = 'Authorization') OR
+        (name = 'reopen' AND entity_type = 'Authorization') OR
+        (name = 'submit' AND entity_type = 'AuthorizationRequestChangelog') OR
+        (name = 'revoke' AND entity_type = 'DenialOfAuthorization') OR
+        (entity_type = 'AuthorizationRequest')
+      )
+    SQL
+  end
+end
