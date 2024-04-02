e69f5cfc82c6b28281ca84b0c6b96892791dae93
Mehdi Farsi <mehdifarsi.pro@gmail.com>
2024-04-02 10:19:00 +0200

    add CreateRevocationOfAuthorizationModel interactor

--- app/organizers/revoke_authorization_request.rb
+++ app/organizers/revoke_authorization_request.rb
@@ -1,10 +1,10 @@
 class RevokeAuthorizationRequest < ApplicationOrganizer
   before do
     context.state_machine_event = :revoke
-    context.event_entity = :denial_of_authorization
+    context.event_entity = :revocation_of_authorization
   end
 
-  organize CreateDenialOfAuthorizationModel,
+  organize CreateRevocationOfAuthorizationModel,
     CreateAuthorizationRequestEventModel,
     ExecuteAuthorizationRequestTransitionWithCallbacks
 end
