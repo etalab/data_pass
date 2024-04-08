945a878961f4c96f8d85cb3d31ded863529a7345
Mehdi Farsi <mehdifarsi.pro@gmail.com>
2024-04-08 09:33:40 +0200

    fix N+1 for authorization_request.organization in Instruction::AuthorizationRequestController#index

--- app/controllers/instruction/authorization_requests_controller.rb
+++ app/controllers/instruction/authorization_requests_controller.rb
@@ -5,7 +5,7 @@ class Instruction::AuthorizationRequestsController < InstructionController
   before_action :extract_authorization_request, except: [:index]
 
   def index
-    @q = policy_scope([:instruction, AuthorizationRequest]).not_archived.ransack(params[:q])
+    @q = policy_scope([:instruction, AuthorizationRequest]).includes([:organization]).not_archived.ransack(params[:q])
     @q.sorts = 'created_at desc' if @q.sorts.empty?
     @authorization_requests = @q.result(distinct: true).page(params[:page])
   end
