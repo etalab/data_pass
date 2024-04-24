6e12269b26aa946ff5216d6dd73eeb266c946219
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-04-24 15:57:27 +0200

    Models HubEEDila
      - Authorization_definitions.yml
      - Authorization_request_forms.yml
      - Model AuthoriationRequest::HubEEDila

---
+++ app/models/authorization_request/hubee_dila.rb
@@ -0,0 +1,7 @@
+class AuthorizationRequest::HubEEDila < AuthorizationRequest
+  add_scopes(validation: {
+    presence: true, if: -> { need_complete_validation?(:scopes) }
+  })
+
+  contact :administrateur_metier, validation_condition: ->(record) { record.need_complete_validation? }
+end
