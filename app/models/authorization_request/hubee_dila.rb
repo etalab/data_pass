8e188251402a64786ff430f78852a66620e2e741
Mehdi Farsi <mehdifarsi.pro@gmail.com>
2024-04-29 15:07:26 +0200

    wip

--- app/models/authorization_request/hubee_dila.rb
+++ app/models/authorization_request/hubee_dila.rb
@@ -3,5 +3,5 @@ class AuthorizationRequest::HubEEDila < AuthorizationRequest
     presence: true, if: -> { need_complete_validation?(:scopes) }
   })
 
-  contact :administrateur_metier, validation_condition: ->(record) { record.need_complete_validation? }
+  contact :administrateur_metier, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
 end
