1eea3b80b2a20e90cc3b2d08258ff94d538a6e32
Loïc Delmaire <loic.delmaire@gmail.com>
2024-05-01 18:38:13 +0200

    Migration: build diff for events v2

--- app/migration/create_diff_from_event.rb
+++ app/migration/create_diff_from_event.rb
@@ -7,8 +7,6 @@ class CreateDiffFromEvent
     @event_row = event_row
     @authorization_request = authorization_request
     fetch_relevant_rows!
-
-    database.close
   end
 
   def perform
@@ -28,9 +26,13 @@ class CreateDiffFromEvent
       when '3'
         print "3\n"
       else
+        raise "Invalid version #{event_diff['_v']}" if event_diff['_v'].present?
+
         handle_v1(final_diff, event_diff)
       end
     end
+
+    database.close
 
     final_diff
   end
@@ -38,22 +40,23 @@ class CreateDiffFromEvent
   private
 
   def handle_v1(final_diff, event_diff)
-    [
-      '_v',
-      'dpo_id',
-      'siret',
-      'organization_id',
-      'nom_raison_sociale',
-      'responsable_traitement_id',
-      'cgu_approved',
-      'dpo_is_informed',
-      'technical_team_type',
-      'technical_team_value',
-      'demarche',
-      'updated_at',
-    ].each do |key|
-      event_diff.delete(key)
-    end
+    clean_keys(
+      event_diff,
+      [
+        '_v',
+        'dpo_id',
+        'siret',
+        'organization_id',
+        'nom_raison_sociale',
+        'responsable_traitement_id',
+        'cgu_approved',
+        'dpo_is_informed',
+        'technical_team_type',
+        'technical_team_value',
+        'demarche',
+        'updated_at',
+      ]
+    )
 
     {
       'dpo' => 'delegue_protection_donnees',
@@ -132,14 +135,82 @@ class CreateDiffFromEvent
   end
 
   def handle_v2(final_diff, event_diff)
-    [
-      '_v',
-      'technical_team_type',
-      'technical_team_value',
-      'cgu_approved',
-      'dpo_is_informed',
-    ].each do |key|
-      event_diff.delete(key)
+    clean_keys(
+      event_diff,
+      [
+        '_v',
+        'technical_team_type',
+        'technical_team_value',
+        'cgu_approved',
+        'demarche',
+        'dpo_is_informed',
+      ]
+    )
+
+    attributes_mapping.each do |old_attribute, new_attribute|
+      value = event_diff.delete(old_attribute)
+
+      next unless authorization_request.class.extra_attributes.include?(new_attribute.to_sym)
+      next if value.nil?
+
+      if value.size == 1
+        value = [nil, value.first]
+      end
+
+      final_diff[new_attribute] = value.map { |v| v.presence.try(:strip) }
+    end
+
+    team_members_diff = event_diff.delete('team_members')
+
+    if team_members_diff
+      team_members = retrieve_team_members_from_database.sort_by { |tm| tm['id'] }
+
+      team_members_diff.delete('_t')
+      team_members_diff.each do |index, team_member_diff|
+        team_member = team_members[index.to_i]
+        next if team_member.blank?
+
+        {
+          'family_name' => 'family_name',
+          'given_name' => 'given_name',
+          'email' => 'email',
+          'phone_number' => 'phone_number',
+          'job' => 'job_title',
+        }.each do |old_attribute, new_attribute|
+          value = team_member_diff.delete(old_attribute)
+
+          next if value.nil?
+
+          if value.size == 1
+            value = [nil, value.first]
+          end
+          byebug if value.is_a?(String)
+
+          final_diff["team_member_#{index}_#{new_attribute}"] = value.map { |v| v.presence.try(:strip) }
+        end
+
+        team_member_diff.delete('user_id')
+
+        byebug if team_member_diff.keys.present?
+      end
+    end
+
+    event_diff.delete('scopes')
+
+    documents = event_diff.delete('documents')
+    if documents
+      documents.delete('_t')
+
+      documents.each do |_, document_diff|
+        case document_diff['type'][0]
+        when 'Document::MaquetteProjet'
+          final_diff['maquette_projet'] = [nil, document_diff['attachment'][0]]
+        when 'Document::LegalBasis'
+          final_diff['cadre_juridique_document'] = [nil, document_diff['attachment'][0]]
+        else
+          byebug
+        end
+      end
     end
 
     byebug if event_diff.keys.present?
@@ -158,6 +229,12 @@ class CreateDiffFromEvent
       'data_retention_period' => 'duree_conservation_donnees_caractere_personnel',
       'data_retention_comment' => 'fixme'
     }
+  end
+
+  def clean_keys(event_diff, keys)
+    keys.each do |key|
+      event_diff.delete(key)
+    end
   end
 
   def fetch_relevant_rows!
@@ -181,5 +258,11 @@ class CreateDiffFromEvent
     end
 
     @relevant_rows.reverse!
+  end
+
+  def retrieve_team_members_from_database
+    @retrieve_team_members_from_database ||= database.execute('select * from team_members where authorization_request_id = ?', event_row['enrollment_id']).map do |row|
+      JSON.parse(row[-1]).to_h
+    end
   end
 end
