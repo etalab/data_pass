99084365d710cfccfd35fff067d4abf1ed913a88
Loïc Delmaire <loic.delmaire@gmail.com>
2024-05-20 14:19:03 +0200

    Migration: fix deploy

--- app/migration/deploy.sh
+++ app/migration/deploy.sh
@@ -5,10 +5,10 @@ RAILS_ENV=production
 ssh -A watchdoge -- RAILS_APP_BRANCH=develop /usr/local/bin/rails_deploy_datapass_reborn_$RAILS_ENV.sh
 scp app/migration/.pgpassword watchdoge:.
 ssh -A watchdoge -- sudo mv .pgpassword /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/
-ssh -A watchdoge -- sudo chown +r /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.pgpassword
-ssh -A watchdoge -- sudo chmod root:root /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.pgpassword
+ssh -A watchdoge -- sudo chown root:root /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.pgpassword
+ssh -A watchdoge -- sudo chmod 644 /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.pgpassword
 
 scp app/migration/.ovh.yml watchdoge:.
 ssh -A watchdoge -- sudo mv .ovh.yml /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/
-ssh -A watchdoge -- sudo chown +r /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
-ssh -A watchdoge -- sudo chmod root:root /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
+ssh -A watchdoge -- sudo chown root:root /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
+ssh -A watchdoge -- sudo chmod 644 /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
