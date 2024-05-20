9cd4b4d8de2eecc8e291df29d0a1bd08c4cef7d6
Loïc Delmaire <loic.delmaire@gmail.com>
2024-05-20 14:24:57 +0200

    Migration: speed up

--- app/migration/deploy.sh
+++ app/migration/deploy.sh
@@ -12,3 +12,6 @@ scp app/migration/.ovh.yml watchdoge:.
 ssh -A watchdoge -- sudo mv .ovh.yml /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/
 ssh -A watchdoge -- sudo chown root:root /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
 ssh -A watchdoge -- sudo chmod 644 /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
+
+
+ssh -A watchdoge -- sudo cp /var/www/datapass_reborn_$RAILS_ENV/data.db /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/dumps/
