7937f4d36f80153cc86d2c89dbe487d894403ef0
Loïc Delmaire <loic.delmaire@gmail.com>
2024-06-07 10:09:58 +0200

    /tableau-de-bord/mentions in subdomain no longers render others authorization requests

--- app/queries/authorization_requests_mentions_query.rb
+++ app/queries/authorization_requests_mentions_query.rb
@@ -5,8 +5,8 @@ class AuthorizationRequestsMentionsQuery
     @user = user
   end
 
-  def perform
-    AuthorizationRequest.where("EXISTS (
+  def perform(relation = AuthorizationRequest.all)
+    relation.where("EXISTS (
       select 1
       from each(authorization_requests.data) as kv
       where kv.key like '%_email' and kv.value = ?
