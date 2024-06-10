7660a198003a98db05529b687577a25377731639
Mehdi Farsi <mehdifarsi.pro@gmail.com>
2024-06-10 15:29:11 +0200

    include AuthorizationExtensions::Modalities in ApiParticulier

--- app/models/authorization_request/api_particulier.rb
+++ app/models/authorization_request/api_particulier.rb
@@ -3,6 +3,7 @@ class AuthorizationRequest::APIParticulier < AuthorizationRequest
   include AuthorizationExtensions::PersonalData
   include AuthorizationExtensions::CadreJuridique
   include AuthorizationExtensions::GDPRContacts
+  include AuthorizationExtensions::Modalities
 
   add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
 
@@ -11,6 +12,10 @@ class AuthorizationRequest::APIParticulier < AuthorizationRequest
 
   add_scopes(validation: {
     presence: true, if: -> { need_complete_validation?(:scopes) }
+  })
+
+  add_modalities(validation: {
+    presence: true, if: -> { need_complete_validation?(:modalities) }
   })
 
   contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
