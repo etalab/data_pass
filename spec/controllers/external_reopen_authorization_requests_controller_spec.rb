7ca2a019b2af90af692f5d449acd73c4310d6fe9
Loïc Delmaire <loic.delmaire@gmail.com>
2024-06-17 12:48:18 +0200

    Introduce proper reopen from external controller
    Previous version did not work because we don't have authorizations' ids
    Moreover, make it more flex by redirecting to authorization if exists,
    even if it's already reopened

---
+++ spec/controllers/external_reopen_authorization_requests_controller_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe ExternalReopenAuthorizationRequestsController do
+  describe 'GET #create' do
+    subject(:reopen_from_external) { get :create, params: { id: authorization_request.id } }
+
+    let(:user) { create(:user) }
+
+    before do
+      session[:user_id] = {
+        'value' => user.id,
+        'expires_at' => 1.hour.from_now,
+      }
+    end
+
+    context 'when the authorization request is not validated' do
+      let(:authorization_request) { create(:authorization_request, applicant: user) }
+
+      it 'does not reopen the authorization request' do
+        expect { reopen_from_external }.not_to change { authorization_request.reload.state }
+      end
+
+      it 'redirects to the authorization summary request page' do
+        reopen_from_external
+
+        expect(response).to redirect_to(summary_authorization_request_form_path(id: authorization_request.id, form_uid: authorization_request.form.uid))
+      end
+    end
+
+    context 'when the authorization request is validated' do
+      let(:authorization_request) { create(:authorization_request, :validated, applicant: user) }
+
+      it 'reopens the authorization request' do
+        expect { reopen_from_external }.to change { authorization_request.reload.state }.from('validated').to('draft')
+      end
+
+      it 'redirects to the authorization summary request page' do
+        reopen_from_external
+
+        expect(response).to redirect_to(summary_authorization_request_form_path(id: authorization_request.id, form_uid: authorization_request.form.uid))
+      end
+    end
+  end
+end
