44e3152fe1c9756ec620160f261864b6baa4a281
Loïc Delmaire <loic.delmaire@gmail.com>
2024-06-19 10:11:13 +0200

    Clean old webhooks serializer
    Does not load in sandbox due to unintialized constant

--- app/serializers/contact_serializer.rb
+++
@@ -1,7 +0,0 @@
-class ContactSerializer < ApplicationSerializer
-  attributes :id, :type, :uid, :email, :given_name, :family_name, :phone_number, :job
-
-  def id  = nil
-  def uid = nil
-  def job = object.job_title
-end
--- app/serializers/webhook_event_serializer.rb
+++
@@ -1,8 +0,0 @@
-class WebhookEventSerializer < ApplicationSerializer
-  attributes :id,
-    :name,
-    :comment,
-    :created_at
-
-  has_one :user, serializer: WebhookUserWithProfileSerializer
-end
