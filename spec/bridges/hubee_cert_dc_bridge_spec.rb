b8c1ba83a71004d3db19fd9cda7aadc7f4bf6fdd
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-07-23 14:22:46 +0200

    Create support hubeeCertDCMocks file with config to remove let mocks in hubee_cert_dc_bridge file

--- spec/bridges/hubee_cert_dc_bridge_spec.rb
+++ spec/bridges/hubee_cert_dc_bridge_spec.rb
@@ -8,65 +8,7 @@ RSpec.describe HubEECertDCBridge do
 
   let(:organization) { create(:organization, id: 3, siret: '21920023500014') }
 
-  let(:organization_payload) do
-    {
-      type: 'SI',
-      companyRegister: '21920023500014',
-      branchCode: '92023',
-      name: 'COMMUNE DE CLAMART',
-      code: nil,
-      country: 'France',
-      postalCode: '92140',
-      territory: 'CLAMART',
-      email: 'admin@yopmail.com',
-      phoneNumber: '0123456789',
-      status: 'Actif'
-    }
-  end
-
   let(:process_code) { 'CERTDC' }
-
-  let(:subscription_body_payload) do
-    {
-      datapassId: 2,
-      processCode: process_code,
-      subscriber: {
-        type: 'SI',
-        companyRegister: '21920023500014',
-        branchCode: '92023',
-      },
-      notificationFrequency: 'unitaire',
-      validateDateTime: '2024-07-18T14:00:55+02:00',
-      updateDateTime: '2024-07-18T14:00:55+02:00',
-      status: 'Inactif',
-      email: 'admin@yopmail.com',
-      localAdministrator: {
-        email: 'admin@yopmail.com',
-      }
-    }
-  end
-
-  let(:subscription_response) do
-    {
-      'accessMode' => nil,
-      'activateDateTime' => nil,
-      'creationDateTime' => '2024-06-14T13:36:20.502+00:00',
-      'datapassId' => 2,
-      'delegationActor' => nil,
-      'email' => 'admin@yopmail.com',
-      'endDateTime' => nil,
-      'id' => '22',
-      'localAdministrator' => { 'email' => 'admin@yopmail.com' },
-      'notificationFrequency' => 'unitaire',
-      'processCode' => 'CERTDC',
-      'rejectDateTime' => nil,
-      'rejectionReason' => nil,
-      'status' => 'Inactif',
-      'subscriber' => { 'branchCode' => '213302722', 'companyRegister' => '21330272200011', 'type' => 'SI' },
-      'updateDateTime' => '2024-07-18T14:00:55+02:00',
-      'validateDateTime' => '2024-07-18T14:00:55+02:00'
-    }
-  end
 
   before do
     allow(HubEEAPIClient).to receive(:new).and_return(hubee_api_client)
@@ -91,7 +33,7 @@ RSpec.describe HubEECertDCBridge do
     end
 
     it 'Create a subscription to hubee with a subscription_body_payload and return a subscription_id' do
-      expect(hubee_api_client).to receive(:create_subscription).with(subscription_body_payload)
+      expect(hubee_api_client).to receive(:create_subscription).with(subscription_body_payload(process_code))
 
       hubee_cert_dc_bridge
     end
---
+++ spec/support/hubee_cert_dc_bridge_mocks.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module HubEECertDCBridgeMocks
+  def organization_payload
+    {
+      type: 'SI',
+      companyRegister: '21920023500014',
+      branchCode: '92023',
+      name: 'COMMUNE DE CLAMART',
+      code: nil,
+      country: 'France',
+      postalCode: '92140',
+      territory: 'CLAMART',
+      email: 'admin@yopmail.com',
+      phoneNumber: '0123456789',
+      status: 'Actif'
+    }
+  end
+
+  def subscription_body_payload(process_code)
+    {
+      datapassId: 2,
+      processCode: process_code,
+      subscriber: {
+        type: 'SI',
+        companyRegister: '21920023500014',
+        branchCode: '92023',
+      },
+      notificationFrequency: 'unitaire',
+      validateDateTime: '2024-07-18T14:00:55+02:00',
+      updateDateTime: '2024-07-18T14:00:55+02:00',
+      status: 'Inactif',
+      email: 'admin@yopmail.com',
+      localAdministrator: {
+        email: 'admin@yopmail.com',
+      }
+    }
+  end
+
+  def subscription_response
+    {
+      'accessMode' => nil,
+      'activateDateTime' => nil,
+      'creationDateTime' => '2024-06-14T13:36:20.502+00:00',
+      'datapassId' => 2,
+      'delegationActor' => nil,
+      'email' => 'admin@yopmail.com',
+      'endDateTime' => nil,
+      'id' => '22',
+      'localAdministrator' => { 'email' => 'admin@yopmail.com' },
+      'notificationFrequency' => 'unitaire',
+      'processCode' => 'CERTDC',
+      'rejectDateTime' => nil,
+      'rejectionReason' => nil,
+      'status' => 'Inactif',
+      'subscriber' => { 'branchCode' => '213302722', 'companyRegister' => '21330272200011', 'type' => 'SI' },
+      'updateDateTime' => '2024-07-18T14:00:55+02:00',
+      'validateDateTime' => '2024-07-18T14:00:55+02:00'
+    }
+  end
+end
