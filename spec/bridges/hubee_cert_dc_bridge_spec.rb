ea2c164a84f512326642c363e9ec568a67bd3c88
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-07-23 09:39:18 +0200

    Fix première salve de Rubocop

--- spec/bridges/hubee_cert_dc_bridge_spec.rb
+++ spec/bridges/hubee_cert_dc_bridge_spec.rb
@@ -3,74 +3,79 @@ RSpec.describe HubEECertDCBridge do
 
   let(:hubee_api_client) { double('HubEEAPIClient') }
 
-  let(:authorization_request) { create(:authorization_request, id: 2, organization_id: organization.id, data: authorization_request_data, last_validated_at: "Thu, 18 Jul 2024 14:00:55.500378000 CEST +02:00", updated_at: "Thu, 18 Jul 2024 14:00:55.500378000 CEST +02:00", linked_token_manager_id: nil) }
+  let(:authorization_request) { create(:authorization_request, id: 2, organization_id: organization.id, data: authorization_request_data, last_validated_at: 'Thu, 18 Jul 2024 14:00:55.500378000 CEST +02:00', updated_at: 'Thu, 18 Jul 2024 14:00:55.500378000 CEST +02:00', linked_token_manager_id: nil) }
   let(:authorization_request_data) { { 'administrateur_metier_email' => 'admin@yopmail.com', 'administrateur_metier_phone_number' => '01.23.45.67.89' } }
 
-  let(:organization) { create(:organization, id: 3, siret: "21920023500014") }
+  let(:organization) { create(:organization, id: 3, siret: '21920023500014') }
 
-  let(:organization_payload) { {
-    type: 'SI',
-    companyRegister: '21920023500014',
-    branchCode: '92023',
-    name: 'COMMUNE DE CLAMART',
-    code: nil,
-    country: 'France',
-    postalCode: '92140',
-    territory: 'CLAMART',
-    email: 'admin@yopmail.com',
-    phoneNumber: '0123456789',
-    status: 'Actif'
-  } }
-
-  let(:process_code) { 'CERTDC' }
-
-  let(:subscription_body_payload) { {
-    datapassId: 2,
-    processCode: process_code,
-    subscriber: {
+  let(:organization_payload) do
+    {
       type: 'SI',
       companyRegister: '21920023500014',
       branchCode: '92023',
-    },
-    notificationFrequency: 'unitaire',
-    validateDateTime: "2024-07-18T14:00:55+02:00",
-    updateDateTime: "2024-07-18T14:00:55+02:00",
-    status: 'Inactif',
-    email: 'admin@yopmail.com',
-    localAdministrator: {
+      name: 'COMMUNE DE CLAMART',
+      code: nil,
+      country: 'France',
+      postalCode: '92140',
+      territory: 'CLAMART',
       email: 'admin@yopmail.com',
+      phoneNumber: '0123456789',
+      status: 'Actif'
     }
-  } }
+  end
 
-  let(:subscription_response) { {
-    "accessMode" => nil,
-    "activateDateTime" => nil,
-    "creationDateTime" => "2024-06-14T13:36:20.502+00:00",
-    "datapassId" => 2,
-    "delegationActor" => nil,
-    "email" => "admin@yopmail.com",
-    "endDateTime" => nil,
-    "id" => "22",
-    "localAdministrator" => { "email" => "admin@yopmail.com" },
-    "notificationFrequency" => "unitaire",
-    "processCode" => "CERTDC",
-    "rejectDateTime" => nil,
-    "rejectionReason" => nil,
-    "status" => "Inactif",
-    "subscriber" => { "branchCode" => "213302722", "companyRegister" => "21330272200011", "type" => "SI" },
-    "updateDateTime" => "2024-07-18T14:00:55+02:00",
-    "validateDateTime" => "2024-07-18T14:00:55+02:00"
-  } }
+  let(:process_code) { 'CERTDC' }
+
+  let(:subscription_body_payload) do
+    {
+      datapassId: 2,
+      processCode: process_code,
+      subscriber: {
+        type: 'SI',
+        companyRegister: '21920023500014',
+        branchCode: '92023',
+      },
+      notificationFrequency: 'unitaire',
+      validateDateTime: '2024-07-18T14:00:55+02:00',
+      updateDateTime: '2024-07-18T14:00:55+02:00',
+      status: 'Inactif',
+      email: 'admin@yopmail.com',
+      localAdministrator: {
+        email: 'admin@yopmail.com',
+      }
+    }
+  end
+
+  let(:subscription_response) do
+    {
+      'accessMode' => nil,
+      'activateDateTime' => nil,
+      'creationDateTime' => '2024-06-14T13:36:20.502+00:00',
+      'datapassId' => 2,
+      'delegationActor' => nil,
+      'email' => 'admin@yopmail.com',
+      'endDateTime' => nil,
+      'id' => '22',
+      'localAdministrator' => { 'email' => 'admin@yopmail.com' },
+      'notificationFrequency' => 'unitaire',
+      'processCode' => 'CERTDC',
+      'rejectDateTime' => nil,
+      'rejectionReason' => nil,
+      'status' => 'Inactif',
+      'subscriber' => { 'branchCode' => '213302722', 'companyRegister' => '21330272200011', 'type' => 'SI' },
+      'updateDateTime' => '2024-07-18T14:00:55+02:00',
+      'validateDateTime' => '2024-07-18T14:00:55+02:00'
+    }
+  end
 
   before do
     allow(HubEEAPIClient).to receive(:new).and_return(hubee_api_client)
-    allow(hubee_api_client).to receive(:get_organization).and_return(organization_payload.with_indifferent_access)
-    allow(hubee_api_client).to receive(:create_subscription).and_return(subscription_response.with_indifferent_access)
+    allow(hubee_api_client).to receive_messages(get_organization: organization_payload.with_indifferent_access, create_subscription: subscription_response.with_indifferent_access)
   end
 
   describe '#perform' do
     it 'does not render en error' do
-      expect { hubee_cert_dc_bridge }.to_not raise_error
+      expect { hubee_cert_dc_bridge }.not_to raise_error
     end
 
     it 'Finds or creates organization' do
@@ -90,7 +95,7 @@ RSpec.describe HubEECertDCBridge do
     end
   end
 
-  describe "#find_or_create_organization" do
+  describe '#find_or_create_organization' do
     context 'when organization exists it calls the hubee API client' do
       it 'To get the organization and return an organization payload' do
         siret = authorization_request.organization.siret
@@ -106,7 +111,7 @@ RSpec.describe HubEECertDCBridge do
         allow(hubee_api_client).to receive(:get_organization).and_raise(Faraday::ResourceNotFound)
       end
 
-      it 'call get_organization but returns a Faraday::ResourceNotFound ' do
+      it 'call get_organization but returns a Faraday::ResourceNotFound' do
         expect(hubee_api_client).to receive(:create_organization).with(organization_payload).and_return(organization_payload)
 
         hubee_cert_dc_bridge
--- spec/services/hubee_api_client_spec.rb
+++ spec/services/hubee_api_client_spec.rb
@@ -1,5 +1,20 @@
 RSpec.describe HubEEAPIClient do
   let(:hubee_api_client) { described_class.new }
+  let(:organization_payload) do
+    {
+      'type' => 'SI',
+      'companyRegister' => '21920023500014',
+      'branchCode' => '92023',
+      'name' => 'COMMUNE DE CLAMART',
+      'code' => nil,
+      'country' => 'France',
+      'postalCode' => '92140',
+      'territory' => 'CLAMART',
+      'email' => 'example@admin.fr',
+      'phoneNumber' => '0123456789',
+      'status' => 'ACTIF'
+    }
+  end
 
   let(:hubee_auth_url) { Rails.application.credentials.hubee_auth_url }
   let(:api_host) { Rails.application.credentials.hubee_host }
@@ -7,22 +22,8 @@ RSpec.describe HubEEAPIClient do
 
   before do
     stub_request(:post, hubee_auth_url)
-      .to_return(status: 200, body: { "access_token" => "access_token" }.to_json, headers: { 'Content-Type' => 'application/json' })
+      .to_return(status: 200, body: { 'access_token' => 'access_token' }.to_json, headers: { 'Content-Type' => 'application/json' })
   end
-
-  let(:organization_payload) { {
-    "type" => "SI",
-    "companyRegister" => "21920023500014",
-    "branchCode" => "92023",
-    "name" => "COMMUNE DE CLAMART",
-    "code" => nil,
-    "country" => "France",
-    "postalCode" => "92140",
-    "territory" => "CLAMART",
-    "email" => "example@admin.fr",
-    "phoneNumber" => "0123456789",
-    "status" => "ACTIF"
-  } }
 
   describe '#get_organization' do
     subject(:get_organization) { hubee_api_client.get_organization(siret, code_commune) }
@@ -34,7 +35,8 @@ RSpec.describe HubEEAPIClient do
       before do
         stub_request(:get, "#{api_host}/referential/v1/organizations/SI-#{siret}-#{code_commune}")
           .with(
-            headers: { 'Authorization' => "Bearer #{access_token}" })
+            headers: { 'Authorization' => "Bearer #{access_token}" }
+          )
           .to_return(status: 200, body: organization_payload.to_json, headers: { 'Content-Type' => 'application/json' })
       end
 
@@ -42,8 +44,7 @@ RSpec.describe HubEEAPIClient do
         get_organization
 
         expect(a_request(:get, "#{api_host}/referential/v1/organizations/SI-#{siret}-#{code_commune}")
-                 .with(headers: { 'Authorization' => "Bearer #{access_token}" })
-        ).to have_been_requested
+                 .with(headers: { 'Authorization' => "Bearer #{access_token}" })).to have_been_requested
       end
 
       it 'returns the hubee organization payload' do
@@ -56,7 +57,8 @@ RSpec.describe HubEEAPIClient do
         before do
           stub_request(:get, "#{api_host}/referential/v1/organizations/SI-#{siret}-#{code_commune}")
             .with(
-              headers: { 'Authorization' => "Bearer #{access_token}" })
+              headers: { 'Authorization' => "Bearer #{access_token}" }
+            )
             .to_return(status: 404, body: '', headers: { 'Content-Type' => 'application/json' })
         end
 
@@ -79,7 +81,8 @@ RSpec.describe HubEEAPIClient do
               'Authorization' => "Bearer #{access_token}",
               'Content-Type' => 'application/json',
               'Tag' => 'Portail HubEE',
-            })
+            }
+          )
           .to_return(status: 200, body: organization_payload.to_json, headers: { 'Content-Type' => 'application/json' })
       end
 
@@ -93,8 +96,8 @@ RSpec.describe HubEEAPIClient do
                      'Authorization' => "Bearer #{access_token}",
                      'Content-Type' => 'application/json',
                      'Tag' => 'Portail HubEE',
-                   })
-        ).to have_been_requested
+                   }
+                 )).to have_been_requested
       end
 
       it 'creates the organization - Render 200' do
@@ -104,7 +107,7 @@ RSpec.describe HubEEAPIClient do
 
     context 'when it renders a 400 Bad Request Error' do
       context 'when there is missing parameters' do
-        let(:response_error_missing_params) { { "errors" => [{ "code" => 400, "message" => "Bad Request : Validation failed.\n[ERROR][REQUEST][POST /referential/v1/organizations @body] Object has missing required properties ([\"branchCode\",\"companyRegister\",\"email\",\"name\",\"postalCode\",\"status\",\"territory\",\"type\"])" }] } }
+        let(:response_error_missing_params) { { 'errors' => [{ 'code' => 400, 'message' => "Bad Request : Validation failed.\n[ERROR][REQUEST][POST /referential/v1/organizations @body] Object has missing required properties ([\"branchCode\",\"companyRegister\",\"email\",\"name\",\"postalCode\",\"status\",\"territory\",\"type\"])" }] } }
 
         before do
           stub_request(:post, "#{api_host}/referential/v1/organizations")
@@ -125,7 +128,7 @@ RSpec.describe HubEEAPIClient do
       end
 
       context 'when it is a conflict' do
-        let(:response_error_organization_already_exists) { { "errors" => [{ "code" => 400, "message" => "Organization SI-21050136700010-00000 already exists" }] } }
+        let(:response_error_organization_already_exists) { { 'errors' => [{ 'code' => 400, 'message' => 'Organization SI-21050136700010-00000 already exists' }] } }
 
         before do
           stub_request(:post, "#{api_host}/referential/v1/organizations")
@@ -150,39 +153,42 @@ RSpec.describe HubEEAPIClient do
   describe '#create_subscription' do
     subject(:create_subscription) { hubee_api_client.create_subscription(subscription_body) }
 
-    let(:subscription_body) { {
-      datapassId: 1,
-      processCode: 'CERTDC',
-      subscriber: {
-        type: 'SI',
-        companyRegister: "21920023500014",
-        branchCode: "92023",
-      },
-      notificationFrequency: 'unitaire',
-      validateDateTime: "2024-07-08T14:31:43+02:00",
-      updateDateTime: "2024-07-08T14:31:43+02:00",
-      status: 'Inactif',
-      email: "example@admin.fr",
-      localAdministrator: {
-        email: "example@admin.fr",
+    let(:subscription_body) do
+      {
+        datapassId: 1,
+        processCode: 'CERTDC',
+        subscriber: {
+          type: 'SI',
+          companyRegister: '21920023500014',
+          branchCode: '92023',
+        },
+        notificationFrequency: 'unitaire',
+        validateDateTime: '2024-07-08T14:31:43+02:00',
+        updateDateTime: '2024-07-08T14:31:43+02:00',
+        status: 'Inactif',
+        email: 'example@admin.fr',
+        localAdministrator: {
+          email: 'example@admin.fr',
+        }
       }
-    } }
+    end
 
     context 'when subscription does not exist' do
-
-      let(:subscription_response) { {
-        "creationDateTime" => "2024-07-11T10:41:04.861+00:00",
-        "datapassId" => 2,
-        "email" => "example@admin.fr",
-        "id" => "d8b00b31-9bb2-4fba-bd55-14776ecdc8f6",
-        "localAdministrator" => { "email" => "example@admin.fr" },
-        "notificationFrequency" => "unitaire",
-        "processCode" => "CERTDC",
-        "status" => "Inactif",
-        "subscriber" => { "branchCode" => "83139", "companyRegister" => "21830139800010", "type" => "SI" },
-        "updateDateTime" => "2024-07-11T10:41:04+0000",
-        "validateDateTime" => "2024-07-08T14:31:43+02:00"
-      } }
+      let(:subscription_response) do
+        {
+          'creationDateTime' => '2024-07-11T10:41:04.861+00:00',
+          'datapassId' => 2,
+          'email' => 'example@admin.fr',
+          'id' => 'd8b00b31-9bb2-4fba-bd55-14776ecdc8f6',
+          'localAdministrator' => { 'email' => 'example@admin.fr' },
+          'notificationFrequency' => 'unitaire',
+          'processCode' => 'CERTDC',
+          'status' => 'Inactif',
+          'subscriber' => { 'branchCode' => '83139', 'companyRegister' => '21830139800010', 'type' => 'SI' },
+          'updateDateTime' => '2024-07-11T10:41:04+0000',
+          'validateDateTime' => '2024-07-08T14:31:43+02:00'
+        }
+      end
 
       before do
         stub_request(:post, "#{api_host}/referential/v1/subscriptions")
@@ -216,7 +222,7 @@ RSpec.describe HubEEAPIClient do
 
     context 'when subscription failed' do
       context 'when subscription already exists' do
-        let(:response) { { "errors" => [{ "code" => 400, "message" => "Subscription CERTDC for organization SI-21920023500014-92023 already exists" }] } }
+        let(:response) { { 'errors' => [{ 'code' => 400, 'message' => 'Subscription CERTDC for organization SI-21920023500014-92023 already exists' }] } }
 
         before do
           stub_request(:post, "#{api_host}/referential/v1/subscriptions")
@@ -225,7 +231,8 @@ RSpec.describe HubEEAPIClient do
               headers: {
                 'Authorization' => "Bearer #{access_token}",
                 'content-type' => 'application/json',
-              })
+              }
+            )
             .to_return(status: 400, body: response.to_json, headers: { 'Content-Type' => 'application/json' })
         end
 
@@ -237,9 +244,9 @@ RSpec.describe HubEEAPIClient do
       context 'when there is missing parameters' do
         let(:response_validation_failed) do
           {
-            "errors" => [{
-              "code" => 400,
-              "message" => "Bad Request : Validation failed.\n[ERROR][REQUEST][POST /referential/v1/subscriptions @body] Instance failed to match all required schemas (matched only 1 out of 2)\n\t* /allOf/0: Object has missing required properties ([\"datapassId\",\"localAdministrator\",\"notificationFrequency\",\"processCode\",\"subscriber\"])\t\n\t- [ERROR] Object has missing required properties ([\"datapassId\",\"localAdministrator\",\"notificationFrequency\",\"processCode\",\"subscriber\"])"
+            'errors' => [{
+              'code' => 400,
+              'message' => "Bad Request : Validation failed.\n[ERROR][REQUEST][POST /referential/v1/subscriptions @body] Instance failed to match all required schemas (matched only 1 out of 2)\n\t* /allOf/0: Object has missing required properties ([\"datapassId\",\"localAdministrator\",\"notificationFrequency\",\"processCode\",\"subscriber\"])\t\n\t- [ERROR] Object has missing required properties ([\"datapassId\",\"localAdministrator\",\"notificationFrequency\",\"processCode\",\"subscriber\"])"
             }]
           }
         end
@@ -251,7 +258,8 @@ RSpec.describe HubEEAPIClient do
               headers: {
                 'Authorization' => "Bearer #{access_token}",
                 'content-type' => 'application/json',
-              })
+              }
+            )
             .to_return(status: 400, body: response_validation_failed.to_json, headers: { 'Content-Type' => 'application/json' })
         end
 
@@ -261,7 +269,7 @@ RSpec.describe HubEEAPIClient do
       end
 
       context 'when there is an Internal server error' do
-        let(:response_error_500) { { "errors" => [{ "code" => 500, "message" => "Internal Server Error" }] } }
+        let(:response_error_500) { { 'errors' => [{ 'code' => 500, 'message' => 'Internal Server Error' }] } }
 
         before do
           stub_request(:post, "#{api_host}/referential/v1/subscriptions")
@@ -270,7 +278,8 @@ RSpec.describe HubEEAPIClient do
               headers: {
                 'Authorization' => "Bearer #{access_token}",
                 'content-type' => 'application/json',
-              })
+              }
+            )
             .to_return(status: 500, body: response_error_500.to_json, headers: {})
         end
 
