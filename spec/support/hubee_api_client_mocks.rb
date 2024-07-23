b9f2e783261dc03509d9841480bc520f4e832b1d
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-07-23 15:22:17 +0200

    Modify mocks to have similar data

--- spec/support/hubee_api_client_mocks.rb
+++ spec/support/hubee_api_client_mocks.rb
@@ -11,7 +11,7 @@ module HubEEAPIClientMocks
       'country' => 'France',
       'postalCode' => '92140',
       'territory' => 'CLAMART',
-      'email' => 'example@admin.fr',
+      'email' => 'admin@yopmail.com',
       'phoneNumber' => '0123456789',
       'status' => 'ACTIF'
     }
@@ -27,29 +27,35 @@ module HubEEAPIClientMocks
         branchCode: '92023',
       },
       notificationFrequency: 'unitaire',
-      validateDateTime: '2024-07-08T14:31:43+02:00',
-      updateDateTime: '2024-07-08T14:31:43+02:00',
+      validateDateTime: '2024-07-18T14:00:55+02:00',
+      updateDateTime: '2024-07-18T14:00:55+02:00',
       status: 'Inactif',
-      email: 'example@admin.fr',
+      email: 'admin@yopmail.com',
       localAdministrator: {
-        email: 'example@admin.fr',
+        email: 'admin@yopmail.com',
       }
     }
   end
 
   def subscription_response
     {
-      'creationDateTime' => '2024-07-11T10:41:04.861+00:00',
+      'accessMode' => nil,
+      'activateDateTime' => nil,
+      'creationDateTime' => '2024-07-18T14:00:55+02:00',
       'datapassId' => 2,
-      'email' => 'example@admin.fr',
-      'id' => 'd8b00b31-9bb2-4fba-bd55-14776ecdc8f6',
-      'localAdministrator' => { 'email' => 'example@admin.fr' },
+      'delegationActor' => nil,
+      'email' => 'admin@yopmail.com',
+      'endDateTime' => nil,
+      'id' => '22',
+      'localAdministrator' => { 'email' => 'admin@yopmail.com' },
       'notificationFrequency' => 'unitaire',
       'processCode' => 'CERTDC',
+      'rejectDateTime' => nil,
+      'rejectionReason' => nil,
       'status' => 'Inactif',
-      'subscriber' => { 'branchCode' => '83139', 'companyRegister' => '21830139800010', 'type' => 'SI' },
-      'updateDateTime' => '2024-07-11T10:41:04+0000',
-      'validateDateTime' => '2024-07-08T14:31:43+02:00'
+      'subscriber' => { 'branchCode' => '213302722', 'companyRegister' => '21330272200011', 'type' => 'SI' },
+      'updateDateTime' => '2024-07-18T14:00:55+02:00',
+      'validateDateTime' => '2024-07-18T14:00:55+02:00'
     }
   end
 end
