3555740bd33865fea4aff03cc6792525192a4b58
Loïc Delmaire <loic.delmaire@gmail.com>
2024-08-12 17:10:34 +0200

    Cucumber: introduce contact métier step
    Will be introduced for API Particulier in a next commit

--- features/step_definitions/authorization_requests_steps.rb
+++ features/step_definitions/authorization_requests_steps.rb
@@ -243,6 +243,14 @@ Quand('je renseigne les informations du contact technique') do
   )
 end
 
+Quand('je renseigne les informations du contact métier') do
+  steps %(
+    * je remplis les informations du contact "Contact métier" avec :
+      | Nom    | Prénom  | Email                | Téléphone   | Fonction    |
+      | Dupont | Louis   | dupont.louis@gouv.fr | 08366565602 | Métier      |
+  )
+end
+
 Quand('je renseigne les informations du délégué à la protection des données') do
   steps %(
     * je remplis les informations du contact "Délégué à la protection des données" avec :
