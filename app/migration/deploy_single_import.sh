c3103059fc276fe6f88bdedd5b8044f66e256ba3
Loïc Delmaire <loic.delmaire@gmail.com>
2024-08-21 15:45:41 +0200

    Add deploy_single_import.sh
    To use with `MainImport.new(authorization_request_ids: ids).perform` in
    production

---
+++ app/migration/deploy_single_import.sh
@@ -0,0 +1,16 @@
+RAILS_ENV=production
+
+[ -f app/migration/dumps/data.db ] && LOCAL=true bundle exec rails runner "ImportDataInLocalDb.new.perform(delete_db_file: false)" || exit 1
+
+scp app/migration/.ovh.yml watchdoge:.
+ssh -A watchdoge -- sudo mv .ovh.yml /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/
+ssh -A watchdoge -- sudo chown root:root /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
+ssh -A watchdoge -- sudo chmod 644 /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
+
+scp app/migration/dumps/data.db watchdoge:dumps/
+scp app/migration/dumps/*.csv watchdoge:dumps/
+
+ssh -A watchdoge -- sudo chown -R root:root dumps/
+ssh -A watchdoge -- sudo chmod -R 644 dumps/
+ssh -A watchdoge -- sudo rm -rf /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/
+ssh -A watchdoge -- sudo cp dumps /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/
