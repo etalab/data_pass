9005f9618f731d5ec53b934d40c34721f16e3a87
Loïc Delmaire <loic.delmaire@gmail.com>
2024-08-27 12:46:33 +0200

    Update deploy_single_import.sh
    After this, run this command in production:
    
    > MainImport.new(authorization_request_ids: [id]).perform

--- app/migration/deploy_single_import.sh
+++ app/migration/deploy_single_import.sh
@@ -7,10 +7,14 @@ ssh -A watchdoge -- sudo mv .ovh.yml /var/www/datapass_reborn_$RAILS_ENV/current
 ssh -A watchdoge -- sudo chown root:root /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
 ssh -A watchdoge -- sudo chmod 644 /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/.ovh.yml
 
+ssh -A watchdoge -- sudo rm -rf dumps/
+ssh -A watchdoge -- mkdir dumps/
+
 scp app/migration/dumps/data.db watchdoge:dumps/
 scp app/migration/dumps/*.csv watchdoge:dumps/
 
 ssh -A watchdoge -- sudo chown -R root:root dumps/
 ssh -A watchdoge -- sudo chmod -R 644 dumps/
-ssh -A watchdoge -- sudo rm -rf /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/
-ssh -A watchdoge -- sudo cp dumps /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/
+ssh -A watchdoge -- sudo rm -rf /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/dumps/
+ssh -A watchdoge -- sudo cp dumps -r /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/
+ssh -A watchdoge -- sudo chmod -R og+r /var/www/datapass_reborn_$RAILS_ENV/current/app/migration/dumps/
