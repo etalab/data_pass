9ce12abdc9c1499d13a24907da43ae86b3cf75bf
Loïc Delmaire <loic.delmaire@gmail.com>
2024-09-06 09:07:50 +0200

    Migration: harden local_reset.sh

--- app/migration/local_reset.sh
+++ app/migration/local_reset.sh
@@ -2,6 +2,7 @@
 
 rm -f app/migration/dumps/*.sql
 rm -f app/migration/dumps/*.csv
+rm -f app/migration/dumps/*.json
 rm -f app/migration/dumps/data.db
 ./app/migration/export_v1.sh
 ./app/migration/export_v2.sh
