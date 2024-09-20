93e287b40bdbe57e66b0376172ad9fed60e360da
Loïc Delmaire <loic.delmaire@gmail.com>
2024-09-20 11:13:34 +0200

    Create bulk update for API Particulier contact metier migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_06_154735) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_06_162647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "pgcrypto"
