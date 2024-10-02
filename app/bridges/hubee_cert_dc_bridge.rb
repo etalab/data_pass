1a54e29cd638beb579c69b27443df674e7f98449
Loïc Delmaire <loic.delmaire@gmail.com>
2024-10-02 16:15:23 +0100

    Harden HubEECertDCBridge
    Mitigate https://errors.data.gouv.fr/organizations/sentry/issues/146403/
    : every single event from this issue is a reopening on a previously
    validated HubEE access.

--- app/bridges/hubee_cert_dc_bridge.rb
+++ app/bridges/hubee_cert_dc_bridge.rb
@@ -10,5 +10,11 @@ class HubEECertDCBridge < HubEEBaseBridge
   def create_and_store_subscription(organization_hubee_payload, process_code)
     subscription_hubee_payload = hubee_api_client.create_subscription(subscription_body(organization_hubee_payload, process_code))
     authorization_request.update!(external_provider_id: subscription_hubee_payload['id'])
+  rescue HubEEAPIClient::AlreadyExistsError
+    raise unless authorization_request.external_provider_id.present? || authorization_request_reopening?
+  end
+
+  def authorization_request_reopening?
+    authorization_request.last_validated_at.present?
   end
 end
