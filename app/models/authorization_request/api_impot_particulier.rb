98735c25f43c9119f8da5743330a0a5d95d9bc8c
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-10-07 15:06:03 +0200

    init form and definition for api impot particulier

---
+++ app/models/authorization_request/api_impot_particulier.rb
@@ -0,0 +1,12 @@
+class AuthorizationRequest::APIImpotParticulier < AuthorizationRequest
+  include AuthorizationExtensions::BasicInfos
+  include AuthorizationExtensions::PersonalData
+  include AuthorizationExtensions::CadreJuridique
+  include AuthorizationExtensions::GDPRContacts
+
+  add_scopes(validation: {
+    presence: true, if: -> { need_complete_validation?(:scopes) }
+  })
+
+  contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+end
