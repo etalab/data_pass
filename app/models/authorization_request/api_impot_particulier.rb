bf24c32b426f801b770ece0706824c6f1452fb1c
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-10-10 17:25:27 +0200

    add maquette, date mep & volumetrie

--- app/models/authorization_request/api_impot_particulier.rb
+++ app/models/authorization_request/api_impot_particulier.rb
@@ -4,6 +4,11 @@ class AuthorizationRequest::APIImpotParticulier < AuthorizationRequest
   include AuthorizationExtensions::CadreJuridique
   include AuthorizationExtensions::GDPRContacts
 
+  add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
+
+  add_attributes :date_prevue_mise_en_production,
+    :volumetrie_approximative
+
   add_scopes(validation: {
     presence: true, if: -> { need_complete_validation?(:scopes) }
   })
