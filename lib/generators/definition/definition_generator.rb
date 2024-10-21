fce1c8f0204f5968dfc460d89f1c1b066403f68a
Loïc Delmaire <loic.delmaire@gmail.com>
2024-10-21 13:25:56 +0100

    Introduce Definition rails generator

---
+++ lib/generators/definition/definition_generator.rb
@@ -0,0 +1,72 @@
+class DefinitionGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  argument :humanized_name, type: :string
+
+  def create_model_file
+    template 'authorization_request.rb.erb', "app/models/authorization_request/#{name.underscore}.rb"
+  end
+
+  def insert_definition_infos
+    append_to_file 'config/authorization_definitions.yml', definition_data
+  end
+
+  def create_forms_file
+    template 'forms.yml.erb', "config/authorization_request_forms/#{name.underscore}.yml"
+  end
+
+  def create_factory_trait
+    inject_into_file 'spec/factories/authorization_requests.rb', before: end_of_factory_file do
+      factory_data
+    end
+  end
+
+  def create_feature_file
+    template 'scenario.feature.erb', "features/habilitations/#{name.underscore}.feature"
+  end
+
+  private
+
+  def definition_data
+    <<-YAML_DATA
+  #{name.underscore}:
+    name: #{humanized_name}
+    description: "FEEDME"
+    provider: "dinum"
+    kind: 'api'
+    link: "https://#{name.underscore.dasherize}.gouv.fr/feedme-with-valid-url"
+    cgu_link: "https://#{name.underscore.dasherize}.gouv.fr/cgu"
+    access_link: "https://#{name.underscore.dasherize}.gouv.fr/tokens/%<external_provider_id>s"
+    public: true
+    blocks:
+      - name: "basic_infos"
+      - name: "personal_data"
+      - name: "legal"
+      - name: "scopes"
+      - name: "contacts"
+    scopes:
+      - name: "Scope 1"
+        value: "value_1"
+    YAML_DATA
+  end
+
+  def factory_data
+    <<-FACTORY_DATA
+
+    trait :#{name.underscore} do
+      type { '#{name.underscore}' }
+
+      form_uid { '#{name.underscore.dasherize}' }
+
+      with_basic_infos
+      with_personal_data
+      with_cadre_juridique
+      with_scopes
+    end
+    FACTORY_DATA
+  end
+
+  def end_of_factory_file
+    /  end\nend/
+  end
+end
---
+++ spec/generator/definition_generator_spec.rb
@@ -0,0 +1,54 @@
+require 'generator_spec'
+require 'generators/definition/definition_generator'
+
+RSpec.describe DefinitionGenerator, type: :generator do
+  arguments ['FranceGenerateur', 'Generateur de France']
+
+  before do
+    import_for_generator('config/authorization_definitions.yml')
+    import_for_generator('spec/factories/authorization_requests.rb')
+
+    run_generator
+  end
+
+  describe 'model file' do
+    subject { generator_file('app/models/authorization_request/france_generateur.rb') }
+
+    it { is_expected.to have_valid_syntax }
+  end
+
+  describe 'definition YAML file' do
+    subject(:file) { generator_file('config/authorization_definitions.yml') }
+
+    it { expect(file.read).to include(/^  france_generateur:/) }
+
+    it 'is a valid YAML' do
+      expect { YAML.load_file(subject) }.not_to raise_error
+    end
+  end
+
+  describe 'forms YAML file' do
+    subject(:file) { generator_file('config/authorization_request_forms/france_generateur.yml') }
+
+    it { expect(file.read).to include(/^france-generateur:/) }
+    it { expect(file.read).to include('authorization_request: FranceGenerateur') }
+
+    it 'is a valid YAML' do
+      expect { YAML.load_file(subject) }.not_to raise_error
+    end
+  end
+
+  describe 'factory file' do
+    subject(:file) { generator_file('spec/factories/authorization_requests.rb') }
+
+    it { is_expected.to have_valid_syntax }
+
+    it { expect(file.read).to include(/trait :france_generateur/) }
+  end
+
+  describe 'feature test file' do
+    subject(:file) { generator_file('features/habilitations/france_generateur.feature') }
+
+    it { expect(file.present?).to be(true) }
+  end
+end
