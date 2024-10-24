b98624ce458b92238a6a06837199b7258ae6e68c
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-10-24 10:44:13 +0200

    Validation
      - Add Validate revenue years selection rules
      - Validation exclusive years combination
      - Validation alidate_incompatible_scopes & some lint
     This is the commit message #2:
    
    Validation exclusive years combination
    
    add validate_incompatible_scopes & some lint
    
    Add validation for incompatible scopes with test associated.

--- app/models/authorization_request/api_impot_particulier.rb
+++ app/models/authorization_request/api_impot_particulier.rb
@@ -13,6 +13,32 @@ class AuthorizationRequest::APIImpotParticulier < AuthorizationRequest
     '1000 appels / minute': 1000,
   }.freeze
 
+  MANDATORY_REVENUE_YEARS = %w[
+    dgfip_annee_n_moins_1
+    dgfip_annee_n_moins_2
+    dgfip_annee_n_moins_3
+    dgfip_annee_n_moins_2_si_indispo_n_moins_1
+  ].freeze
+
+  EXCLUSIVE_REVENUE_YEARS = [
+    %w[dgfip_annee_n_moins_2_si_indispo_n_moins_1],
+    %w[dgfip_annee_n_moins_1 dgfip_annee_n_moins_2 dgfip_annee_n_moins_3]
+  ].freeze
+
+  INCOMPATIBLE_SCOPES = [
+    %w[dgfip_annee_n_moins_2_si_indispo_n_moins_1 dgfip_annee_df_au_3112_si_deces_ctb_mp],
+    %w[
+      dgfip_indiIFI dgfip_RevDecl_Cat1_Tspr dgfip_RevDecl_Cat5_NonSal
+      dgfip_RevNets_Cat1_Tspr dgfip_RevNets_Cat1_RentOn dgfip_RevNets_Cat2_Rcm
+      dgfip_RevNets_Cat3_PMV dgfip_RevNets_Cat4_Ref dgfip_RevNets_Cat5_NonSal
+      dgfip_PaDeduc_EnfMaj dgfip_PaDeduc_Autres dgfip_EpargRetrDeduc dgfip_IndLep
+    ]
+  ].freeze
+
+  validate :validate_revenue_years_selection,
+    :validate_exclusive_years_scope_combination,
+    :validate_incompatible_scopes
+
   add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
 
   add_attributes :date_prevue_mise_en_production
@@ -22,4 +48,28 @@ class AuthorizationRequest::APIImpotParticulier < AuthorizationRequest
   })
 
   contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+
+  private
+
+  def validate_revenue_years_selection
+    return if scopes.intersect?(MANDATORY_REVENUE_YEARS)
+
+    errors.add(:scopes, :invalid, message: 'sont invalides : Vous devez cocher au moins une année de revenus souhaitée avant de continuer')
+  end
+
+  def validate_exclusive_years_scope_combination
+    return unless scope_exists_in_each_arrays?(*EXCLUSIVE_REVENUE_YEARS)
+
+    errors.add(:scopes, :invalid, message: "sont invalides : Vous ne pouvez pas sélectionner la donnée 'avant dernière année de revenu, si la dernière année de revenu est indisponible' avec d'autres années de revenus")
+  end
+
+  def validate_incompatible_scopes
+    return unless scope_exists_in_each_arrays?(*INCOMPATIBLE_SCOPES)
+
+    errors.add(:scopes, :invalid, message: 'sont invalides : Des données incompatibles entre elles ont été cochées. Pour connaître les modalités d’appel et de réponse de l’API Impôt particulier ainsi que les données proposées, vous pouvez consulter le guide de présentation de cette API dans la rubrique « Les données nécessaires > Comment choisir les données »')
+  end
+
+  def scope_exists_in_each_arrays?(array_1, array_2)
+    scopes.intersect?(array_1) && scopes.intersect?(array_2)
+  end
 end
--- spec/models/authorization_request/api_impot_particulier_spec.rb
+++ spec/models/authorization_request/api_impot_particulier_spec.rb
@@ -1,4 +1,14 @@
 RSpec.describe AuthorizationRequest::APIImpotParticulier, type: :model do
+  subject { build(:authorization_request, :api_impot_particulier_editeur, scopes: scopes) }
+
+  shared_examples 'valid scope' do
+    it { is_expected.to be_valid }
+  end
+
+  shared_examples 'invalid scope' do
+    it { is_expected.not_to be_valid }
+  end
+
   describe 'volumetrie validation' do
     subject(:authorization_request) { build(:authorization_request, :api_impot_particulier_editeur, fill_all_attributes: true, volumetrie_appels_par_minute:, volumetrie_justification:) }
 
@@ -43,6 +53,68 @@ RSpec.describe AuthorizationRequest::APIImpotParticulier, type: :model do
       let(:safety_certification_end_date) { Date.yesterday }
 
       it { is_expected.not_to be_valid }
+    end
+  end
+
+  describe 'scopes validation of revenue years' do
+    AuthorizationRequest::APIImpotParticulier::MANDATORY_REVENUE_YEARS.each do |year|
+      context "with mandatory revenue year scope #{year}" do
+        let(:scopes) { [year] }
+
+        include_examples 'valid scope'
+      end
+    end
+
+    context 'without any revenue years scope selected' do
+      let(:scopes) { %w[dgfip_indiIFI] }
+
+      include_examples 'invalid scope'
+    end
+  end
+
+  describe 'validation of exclusive revenue years scope combination' do
+    AuthorizationRequest::APIImpotParticulier::EXCLUSIVE_REVENUE_YEARS.each do |exclusive_group|
+      context "with valid exclusive years scope combination: #{exclusive_group.join(', ')}" do
+        let(:scopes) { exclusive_group }
+
+        include_examples 'valid scope'
+      end
+    end
+
+    context 'with invalid exclusive revenue years scope combination' do
+      let(:scopes) { %w[dgfip_annee_n_moins_2_si_indispo_n_moins_1 dgfip_annee_n_moins_1] }
+
+      include_examples 'invalid scope'
+    end
+  end
+
+  describe 'validation with incompatible scopes' do
+    incompatible_combinations = AuthorizationRequest::APIImpotParticulier::INCOMPATIBLE_SCOPES.combination(2).map(&:flatten)
+
+    incompatible_combinations.each do |incompatible_scopes|
+      context "with incompatible scopes combination: #{incompatible_scopes.join(', ')}" do
+        let(:scopes) { incompatible_scopes }
+
+        include_examples 'invalid scope'
+      end
+    end
+
+    context 'with valid scopes combination from array_1' do
+      let(:scopes) { %w[dgfip_annee_df_au_3112_si_deces_ctb_mp dgfip_annee_n_moins_2_si_indispo_n_moins_1] }
+
+      include_examples 'valid scope'
+    end
+
+    context 'with invalid complex incompatible scopes combination from array_1 and array_2' do
+      let(:scopes) { %w[dgfip_annee_df_au_3112_si_deces_ctb_mp dgfip_annee_n_moins_2_si_indispo_n_moins_1 dgfip_indiIFI dgfip_RevDecl_Cat1_Tspr] }
+
+      include_examples 'invalid scope'
+    end
+
+    context 'with valid incompatible scopes combination with revenue years scope' do
+      let(:scopes) { %w[dgfip_annee_n_moins_1 dgfip_indiIFI] }
+
+      include_examples 'valid scope'
     end
   end
 end
