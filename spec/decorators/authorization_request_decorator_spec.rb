488c289965f84ff964998408fd8602cb3112d079
Loïc Delmaire <loic.delmaire@gmail.com>
2024-10-24 16:59:47 +0100

    Add tests for AuthorizationRequestDecorator#prefilled_data?

---
+++ spec/decorators/authorization_request_decorator_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe AuthorizationRequestDecorator, type: :decorator do
+  describe '#prefilled_data?' do
+    subject { authorization_request.decorate.prefilled_data?(keys) }
+
+    let(:authorization_request) { create(:authorization_request, authorization_request_form, fill_all_attributes: true) }
+    let(:keys) { authorization_request.data.keys }
+
+    context 'with a simple authorization request which has no prefilled_data nor scopes on his config' do
+      let(:authorization_request_form) { :hubee_cert_dc }
+
+      it { is_expected.to be(false) }
+    end
+
+    context 'with an authorization request which has prefilled data' do
+      let(:authorization_request_form) { :api_entreprise_mgdis }
+
+      context 'when keys match prefilled data' do
+        let(:keys) { %w[intitule] }
+
+        it { is_expected.to be(true) }
+      end
+
+      context 'when keys do not match prefilled data' do
+        let(:keys) { %w[responsable_traitement_email] }
+
+        it { is_expected.to be(false) }
+      end
+    end
+  end
+end
