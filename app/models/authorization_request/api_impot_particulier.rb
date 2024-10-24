bf6ddb41078825eb4adb4dbdbe45628be74c22d0
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-10-24 10:56:41 +0200

    Validation exclusive years combination

--- app/models/authorization_request/api_impot_particulier.rb
+++ app/models/authorization_request/api_impot_particulier.rb
@@ -6,7 +6,15 @@ class AuthorizationRequest::APIImpotParticulier < AuthorizationRequest
   include AuthorizationExtensions::OperationalAcceptance
   include AuthorizationExtensions::SafetyCertification
 
-  validate :validate_revenue_years_selection
+  SELECTED_REVENUE_YEARS = %w[
+    dgfip_annee_n_moins_1
+    dgfip_annee_n_moins_2
+    dgfip_annee_n_moins_3
+    dgfip_annee_n_moins_2_si_indispo_n_moins_1
+  ].freeze
+
+  validate :validate_revenue_years_selection,
+    :validate_exclusive_years_scope_combination
 
   add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
 
@@ -16,21 +24,24 @@ class AuthorizationRequest::APIImpotParticulier < AuthorizationRequest
     presence: true, if: -> { need_complete_validation?(:scopes) }
   })
 
-  add_document :specific_requirements_document, content_type: %w[application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet], size: { less_than: 10.megabytes }
-  add_attributes :specific_requirements, validation: { presence: true, if: -> { specific_requirements_document.present? } }
+  # add_attributes :specific_requirements
+  # add_document :specific_requirements_document, content_type: %w[application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet], size: { less_than: 10.megabytes },
+  #   validation: { presence: true, if: -> { specific_requirements.present? } }
 
   contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
 
   private
 
   def validate_revenue_years_selection
-    selected_years = %w[
-      dgfip_annee_n_moins_1 dgfip_annee_n_moins_2 dgfip_annee_n_moins_3
-      dgfip_annee_n_moins_2_si_indispo_n_moins_1
-    ]
-
-    return unless (scopes & selected_years).empty?
+    return unless (scopes & SELECTED_REVENUE_YEARS).empty?
 
     errors.add(:scopes, :invalid, message: "sont invalides : Vous devez cocher au moins une année de revenus souhaitée avant de continuer")
+  end
+
+  def validate_exclusive_years_scope_combination
+    if (scopes & %w[dgfip_annee_n_moins_2_si_indispo_n_moins_1]).present? &&
+      (scopes & %w[dgfip_annee_n_moins_1 dgfip_annee_n_moins_2 dgfip_annee_n_moins_3]).present?
+      errors.add(:scopes, :invalid, message: "sont invalides : Vous ne pouvez pas sélectionner la donnée 'avant dernière année de revenu, si la dernière année de revenu est indisponible' avec d'autres années de revenus")
+    end
   end
 end
--- spec/models/authorization_request/api_impot_particulier_spec.rb
+++ spec/models/authorization_request/api_impot_particulier_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe AuthorizationRequest::APIImpotParticulier, type: :model do
-  describe 'scopes validation of revenue years ' do
-    subject { build(:authorization_request, :api_impot_particulier_editeur, scopes: scopes) }
+  subject { build(:authorization_request, :api_impot_particulier_editeur, scopes: scopes) }
 
+  describe 'scopes validation of revenue years ' do
     context 'with revenue years scope selected' do
       let(:scopes) { %w[dgfip_annee_n_moins_1] }
 
@@ -15,7 +15,17 @@ RSpec.describe AuthorizationRequest::APIImpotParticulier, type: :model do
     end
   end
 
-  describe '' do
+  describe 'validation of exclusive years scope combination' do
+    context 'with exclusive years scope combination' do
+      let(:scopes) { %w[dgfip_annee_n_moins_1 dgfip_annee_n_moins_2] }
 
+      it { is_expected.to be_valid }
+    end
+
+    context 'with invalid exclusive years scope combination' do
+      let(:scopes) { %w[dgfip_annee_n_moins_2_si_indispo_n_moins_1 dgfip_annee_n_moins_1] }
+
+      it { is_expected.not_to be_valid }
+    end
   end
 end
