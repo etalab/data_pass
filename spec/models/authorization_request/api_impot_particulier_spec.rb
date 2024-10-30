30165399438c6fd3a35ea01bc28cc948e7f182f2
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-10-30 10:01:36 +0100

    Add a Describe scope validation to encapsulate all validation test

--- spec/models/authorization_request/api_impot_particulier_spec.rb
+++ spec/models/authorization_request/api_impot_particulier_spec.rb
@@ -50,71 +50,73 @@ RSpec.describe AuthorizationRequest::APIImpotParticulier, type: :model do
     end
   end
 
-  describe 'scopes validation of revenue years' do
-    before { authorization_request.current_build_step = 'scopes' }
+  describe 'scopes validation' do
+    describe 'scopes validation of revenue years' do
+      before { authorization_request.current_build_step = 'scopes' }
 
-    AuthorizationRequest::APIImpotParticulier::MANDATORY_REVENUE_YEARS.each do |year|
-      context "with mandatory revenue year scope #{year}" do
-        let(:scopes) { [year] }
+      AuthorizationRequest::APIImpotParticulier::MANDATORY_REVENUE_YEARS.each do |year|
+        context "with mandatory revenue year scope #{year}" do
+          let(:scopes) { [year] }
 
-        it { is_expected.to be_valid }
+          it { is_expected.to be_valid }
+        end
       end
-    end
 
-    context 'without any revenue years scope selected' do
-      let(:scopes) { %w[dgfip_indiIFI] }
-
-      it { is_expected.not_to be_valid }
-    end
-  end
-
-  describe 'validation of exclusive revenue years scope combination' do
-    before { authorization_request.current_build_step = 'scopes' }
-
-    AuthorizationRequest::APIImpotParticulier::EXCLUSIVE_REVENUE_YEARS.each do |exclusive_group|
-      context "with valid exclusive years scope combination: #{exclusive_group.join(', ')}" do
-        let(:scopes) { exclusive_group }
-
-        it { is_expected.to be_valid }
-      end
-    end
-
-    context 'with invalid exclusive revenue years scope combination' do
-      let(:scopes) { %w[dgfip_annee_n_moins_2_si_indispo_n_moins_1 dgfip_annee_n_moins_1] }
-
-      it { is_expected.not_to be_valid }
-    end
-  end
-
-  describe 'validation with incompatible scopes' do
-    before { authorization_request.current_build_step = 'scopes' }
-
-    incompatible_combinations = AuthorizationRequest::APIImpotParticulier::INCOMPATIBLE_SCOPES.combination(2).map(&:flatten)
-
-    incompatible_combinations.each do |incompatible_scopes|
-      context "with incompatible scopes combination: #{incompatible_scopes.join(', ')}" do
-        let(:scopes) { incompatible_scopes }
+      context 'without any revenue years scope selected' do
+        let(:scopes) { %w[dgfip_indiIFI] }
 
         it { is_expected.not_to be_valid }
       end
     end
 
-    context 'with valid scopes combination from array_1' do
-      let(:scopes) { %w[dgfip_annee_df_au_3112_si_deces_ctb_mp dgfip_annee_n_moins_2_si_indispo_n_moins_1] }
+    describe 'validation of exclusive revenue years scope combination' do
+      before { authorization_request.current_build_step = 'scopes' }
 
-      it { is_expected.to be_valid }
+      AuthorizationRequest::APIImpotParticulier::EXCLUSIVE_REVENUE_YEARS.each do |exclusive_group|
+        context "with valid exclusive years scope combination: #{exclusive_group.join(', ')}" do
+          let(:scopes) { exclusive_group }
+
+          it { is_expected.to be_valid }
+        end
+      end
+
+      context 'with invalid exclusive revenue years scope combination' do
+        let(:scopes) { %w[dgfip_annee_n_moins_2_si_indispo_n_moins_1 dgfip_annee_n_moins_1] }
+
+        it { is_expected.not_to be_valid }
+      end
     end
 
-    context 'with invalid complex incompatible scopes combination from array_1 and array_2' do
-      let(:scopes) { %w[dgfip_annee_df_au_3112_si_deces_ctb_mp dgfip_annee_n_moins_2_si_indispo_n_moins_1 dgfip_indiIFI dgfip_RevDecl_Cat1_Tspr] }
+    describe 'validation with incompatible scopes' do
+      before { authorization_request.current_build_step = 'scopes' }
 
-      it { is_expected.not_to be_valid }
-    end
+      incompatible_combinations = AuthorizationRequest::APIImpotParticulier::INCOMPATIBLE_SCOPES.combination(2).map(&:flatten)
 
-    context 'with valid incompatible scopes combination with revenue years scope' do
-      let(:scopes) { %w[dgfip_annee_n_moins_1 dgfip_indiIFI] }
+      incompatible_combinations.each do |incompatible_scopes|
+        context "with incompatible scopes combination: #{incompatible_scopes.join(', ')}" do
+          let(:scopes) { incompatible_scopes }
 
-      it { is_expected.to be_valid }
+          it { is_expected.not_to be_valid }
+        end
+      end
+
+      context 'with valid scopes combination from array_1' do
+        let(:scopes) { %w[dgfip_annee_df_au_3112_si_deces_ctb_mp dgfip_annee_n_moins_2_si_indispo_n_moins_1] }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'with invalid complex incompatible scopes combination from array_1 and array_2' do
+        let(:scopes) { %w[dgfip_annee_df_au_3112_si_deces_ctb_mp dgfip_annee_n_moins_2_si_indispo_n_moins_1 dgfip_indiIFI dgfip_RevDecl_Cat1_Tspr] }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'with valid incompatible scopes combination with revenue years scope' do
+        let(:scopes) { %w[dgfip_annee_n_moins_1 dgfip_indiIFI] }
+
+        it { is_expected.to be_valid }
+      end
     end
   end
 end
