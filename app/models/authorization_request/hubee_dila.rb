c00c71cd252d73fca79ef691ba2aad50080b16f4
Loïc Delmaire <loic.delmaire@gmail.com>
2024-11-04 10:28:00 +0000

    FIX AuthorizationRequest::HubEEDila#unique_scope_request
    Detected through strange rubocop fix suggestion
    
    Related https://github.com/etalab/data_pass/pull/520#issuecomment-2454323327

--- app/models/authorization_request/hubee_dila.rb
+++ app/models/authorization_request/hubee_dila.rb
@@ -10,18 +10,16 @@ class AuthorizationRequest::HubEEDila < AuthorizationRequest
   private
 
   def unique_scope_request
-    existing_requests = self.class.where.not(state: 'archived')
+    existing_requests = self.class
+      .where.not(state: 'archived')
       .where(
         organization:,
         type: 'AuthorizationRequest::HubEEDila',
-        state: %w[draft validated submitted changes_requested]
       )
 
     existing_scopes = existing_requests.map(&:scopes).flatten.uniq
 
-    required_scopes = %w[etat_civil depot_dossier_pacs recensement_citoyen hebergement_tourisme je_change_de_coordonnees]
-
-    return unless (required_scopes & existing_scopes) == required_scopes
+    return unless existing_scopes.intersect?(scopes)
 
     errors.add(:base, 'Une demande d‘habilitation avec les mêmes scopes existe déjà pour cette organisation.')
   end
