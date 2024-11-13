3be2b414cc5cb2bdaf5cec59786952fef612481c
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-11-13 11:23:36 +0100

    scout commit

--- spec/factories/authorization_requests.rb
+++ spec/factories/authorization_requests.rb
@@ -198,7 +198,7 @@ FactoryBot.define do
 
     trait :with_scopes do
       after(:build) do |authorization_request, evaluator|
-        if authorization_request.need_complete_validation? || (evaluator.fill_all_attributes)
+        if authorization_request.need_complete_validation? || evaluator.fill_all_attributes
           next if authorization_request.scopes.any?
 
           authorization_request.scopes ||= []
