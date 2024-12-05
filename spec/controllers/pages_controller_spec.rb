3f6bd2023d3663374a01ab67481e07fe0daf7ae8
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-12-05 15:39:04 +0100

    Display impot particulier cgu's bas a sable and production
      - In order to display api impot particulier environment bac a sable FC and no FC, we need to create a static page.
        Introduce the need to access the page without beeing connected with allow_unauthenticated_access.
    
    Create another page to production CGUS
    
      - controller action / view / routes
    
    Add Link to CGU's into config file.
    
    Add some test for static controller
    
    Improve wording pages
    
    We already have a static page but nammed pages... .
    
      - Delete static page controller
      - Move methods in pages controller
      - Routes according to changes
    
    Add spec to changes

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe PagesController, type: :controller do
+  describe 'GET #cgu_api_impot_particulier_bas' do
+    it 'returns a 200 response' do
+      get :cgu_api_impot_particulier_bas
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET #cgu_api_impot_particulier_prod' do
+    it 'returns a 200 response' do
+      get :cgu_api_impot_particulier_prod
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
