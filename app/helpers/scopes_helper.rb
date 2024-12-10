c89c046816b3d9d86377b693554060b2650894e8
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-12-10 12:16:40 +0100

    Modification of case group_name in scopes_helper

--- app/helpers/scopes_helper.rb
+++ app/helpers/scopes_helper.rb
@@ -3,11 +3,18 @@ module ScopesHelper
     messages = []
 
     case group_name
-    when 'Années sur lesquelles porte votre demande'
-      messages << authorization_request.errors[:revenue_years] if authorization_request.errors[:revenue_years].any?
-      messages << authorization_request.errors[:revenue_years_compatibility] if authorization_request.errors[:revenue_years_compatibility].any?
+      when 'Années sur lesquelles porte votre demande'
+        messages << authorization_request.errors[:revenue_years] if authorization_request.errors[:revenue_years].any?
+        messages << authorization_request.errors[:revenue_years_compatibility] if authorization_request.errors[:revenue_years_compatibility].any?
+      when *DGFIPExtensions::APIImpotParticulierScopes::SCOPE_ERROR_MESSAGES[:income_data_incompatibility][:groups]
+        messages << authorization_request.errors[:income_data_incompatibility] if authorization_request.errors[:income_data_incompatibility].any?
     end
 
     messages.flatten
   end
+
+  def specific_requirements_error(authorization_request)
+    authorization_request.errors[:specific_requirements] if authorization_request.errors[:specific_requirements].any?
+  end
+
 end
