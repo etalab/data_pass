9c4d554ae25bf18ad0e3a5984dd8852308f2e076
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-12-10 11:25:03 +0100

    Create a scope module helper to simplify view and make it more flexible in view and with differents api forms
      - with modification of view too

--- app/helpers/scopes_helper.rb
+++ app/helpers/scopes_helper.rb
@@ -1,4 +1,13 @@
-# frozen_string_literal: true
-
 module ScopesHelper
+  def error_messages_for_scope_group(authorization_request, group_name)
+    messages = []
+
+    case group_name
+    when 'Années sur lesquelles porte votre demande'
+      messages << authorization_request.errors[:revenue_years] if authorization_request.errors[:revenue_years].any?
+      messages << authorization_request.errors[:revenue_years_compatibility] if authorization_request.errors[:revenue_years_compatibility].any?
+    end
+
+    messages.flatten
+  end
 end
