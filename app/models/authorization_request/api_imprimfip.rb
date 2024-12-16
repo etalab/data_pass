01b3ef571dd8e311de46e3ba1bb5381b685b935a
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-12-16 16:47:23 +0100

    API IMPRIM'FIP
    API IMPRIM'FIP

---
+++ app/models/authorization_request/api_imprimfip.rb
@@ -0,0 +1,22 @@
+class AuthorizationRequest::APIImprimfip < AuthorizationRequest
+  include AuthorizationExtensions::BasicInfos
+  include AuthorizationExtensions::PersonalData
+  include AuthorizationExtensions::CadreJuridique
+  include AuthorizationExtensions::GDPRContacts
+
+  include AuthorizationExtensions::OperationalAcceptance
+  include AuthorizationExtensions::SafetyCertification
+  include AuthorizationExtensions::Volumetrie
+
+  VOLUMETRIES = {
+    '50 appels / minute': 50,
+    '200 appels / minute': 200,
+    '1000 appels / minute': 1000,
+  }.freeze
+
+  add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
+
+  add_attributes :date_prevue_mise_en_production
+
+  contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+end
