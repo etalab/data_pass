fc014b9c6681ee7cf80110a806b13d2c316d338c
Loïc Delmaire <loic.delmaire@gmail.com>
2024-12-17 08:47:53 +0100

    Instruction: can manage authorization request instructor drafts
    Only model management for now, no invite nor applicant flow there
    
    Please read the feature spec associated for full specification

---
+++ app/policies/instruction/authorization_request_instructor_draft_policy.rb
@@ -0,0 +1,54 @@
+class Instruction::AuthorizationRequestInstructorDraftPolicy < ApplicationPolicy
+  def enable?
+    %w[
+      api_entreprise
+      api_particulier
+    ].any? do |authorization_definition_id|
+      user.instructor?(authorization_definition_id)
+    end
+  end
+
+  def show?
+    record.authorization_request_class.in?(current_user_instructor_types)
+  end
+
+  def edit?
+    show?
+  end
+
+  def update?
+    show?
+  end
+
+  def destroy?
+    show?
+  end
+
+  private
+
+  def current_user_instructor_types
+    current_user_instructor_roles.map do |scope|
+      "AuthorizationRequest::#{scope.split(':').first.classify}"
+    end
+  end
+
+  def current_user_instructor_roles
+    user.instructor_roles
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.where(authorization_request_class: current_user_instructor_types)
+    end
+
+    def current_user_instructor_types
+      current_user_instructor_roles.map do |scope|
+        "AuthorizationRequest::#{scope.split(':').first.classify}"
+      end
+    end
+
+    def current_user_instructor_roles
+      user.instructor_roles
+    end
+  end
+end
