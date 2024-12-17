ba17d1d1fc9905d547be1c904193a64a8d3f56e5
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-12-17 18:24:50 +0100

    Scout commit

--- app/models/authorization_request/api_ocfi.rb
+++ app/models/authorization_request/api_ocfi.rb
@@ -14,7 +14,7 @@ class AuthorizationRequest::APIOcfi < AuthorizationRequest
   add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
 
   add_attributes :date_prevue_mise_en_production,
-                 :volumetrie_approximative
+    :volumetrie_approximative
 
   contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
 end
