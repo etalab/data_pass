e0689706abd1083e18fe36a00a085857d10f0789
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-01-16 17:48:18 +0100

    Add API Ficoba base

---
+++ app/models/authorization_request/api_ficoba_sandbox.rb
@@ -0,0 +1,22 @@
+class AuthorizationRequest::APIFicobaSandbox < AuthorizationRequest
+  include AuthorizationExtensions::BasicInfos
+  include AuthorizationExtensions::PersonalData
+  include AuthorizationExtensions::CadreJuridique
+  include AuthorizationExtensions::GDPRContacts
+  include AuthorizationExtensions::Modalities
+
+  MODALITIES = %w[with_iban with_spi with_siren with_personne_physique with_personne_morale with_adresse with_iban_siren with_iban_personne_physique with_iban_personne_morale].freeze
+
+  add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
+
+  add_attributes :date_prevue_mise_en_production,
+                 :volumetrie_approximative
+
+  add_scopes(validation: {
+    presence: true, if: -> { need_complete_validation?(:scopes) }
+  })
+
+  contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+
+  add_checkbox :dpd_homologation_checkbox
+end
--- spec/factories/authorization_requests.rb
+++ spec/factories/authorization_requests.rb
@@ -1125,5 +1125,34 @@ FactoryBot.define do
       with_operational_acceptance
       with_volumetrie
     end
+
+    trait :api_ficoba_sandbox do
+      type { 'AuthorizationRequest::APIFicobaSandbox' }
+
+      form_uid { 'api-ficoba-sandbox' }
+
+      with_basic_infos
+      with_personal_data
+      with_cadre_juridique
+      with_modalities
+      with_scopes
+    end
+
+    trait :api_ficoba_production do
+      type { 'AuthorizationRequest::APIFicoba' }
+
+      form_uid { 'api-ficoba-production' }
+
+      has_previous_authorization_validated
+
+      with_basic_infos
+      with_personal_data
+      with_cadre_juridique
+      with_modalities
+      with_scopes
+      with_safety_certification
+      with_operational_acceptance
+      with_volumetrie
+    end
   end
 end
