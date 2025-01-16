305d68019625048efecc051241afbf795febdac6
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-01-16 13:42:19 +0100

    Add missing checkbox for api_impot_part_sandbox forms

--- app/models/authorization_request/api_impot_particulier_sandbox.rb
+++ app/models/authorization_request/api_impot_particulier_sandbox.rb
@@ -18,4 +18,6 @@ class AuthorizationRequest::APIImpotParticulierSandbox < AuthorizationRequest
   })
 
   contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+
+  add_checkbox :dpd_homologation_checkbox
 end
