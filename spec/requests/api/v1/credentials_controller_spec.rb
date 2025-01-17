48f712a09a7df617c16fda3a1bab536aea43143d
Loïc Delmaire <loic.delmaire@gmail.com>
2025-01-17 16:48:19 +0100

    Scout commit

--- spec/requests/api/v1/credentials_controller_spec.rb
+++ spec/requests/api/v1/credentials_controller_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'Credentials', type: :request do
+RSpec.describe 'API: Credentials', type: :request do
   context 'when unauthorized' do
     it 'returns unauthorized' do
       get '/api/v1/me'
