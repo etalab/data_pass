0f8b9826972fd006fdc88b35fd76bdf0d3c8bea8
Delmaire Loïc <skelz0r@gmail.com>
2025-01-21 10:12:09 +0100

    Migration: wip main script

--- app/migration/main_import.rb
+++ app/migration/main_import.rb
@@ -22,7 +22,7 @@ class MainImport
       valid_authorization_request_ids = authorization_requests.pluck(:id)
     end
 
-    import(:authorization_request_events, { dump_sql: ENV['DUMP'] == 'true', valid_authorization_request_ids: })
+    # import(:authorization_request_events, { dump_sql: ENV['DUMP'] == 'true', valid_authorization_request_ids: })
 
     %i[warned skipped].each do |kind|
       export(kind)
@@ -54,10 +54,10 @@ class MainImport
 
     data = public_send(kind)
     CSV.open(export_path(kind), 'w') do |csv|
-      csv << %w[id target_api kind]
+      csv << %w[id target_api kind url]
 
       data.each do |datum|
-        csv << [datum.id, datum.target_api, datum.kind]
+        csv << [datum.id, datum.target_api, datum.kind, "https://datapass.api.gouv.fr/#{datum.target_api.gsub('_', '-')}/#{datum.id}"]
       end
     end
   end
@@ -86,10 +86,17 @@ class MainImport
     {
       authorization_requests_filter: ->(enrollment_row) do
         %w[
+          64447 64348 62067 6195 6033 5055 4438 4436 4433 4198 4166 4162 4159 4157 4155
+          4153 4151 4149 4147 4145 4143 4140 4138 4136 4134 4132 4130 4127 4125 4123
+          4121 4119 4117 4115 4113 4109 4106 4104 4101 4098 4096 4094 4092 4090 4088
+          3932 3540 54510 54497 43297 40647 40116 39580 14323 51640 49730 49191 48323
+          23657 14770 45463 45442 43965 43816 5893 4442 45173 3666
         ].exclude?(enrollment_row['id'])
+        # %w[63955 64669].include?(enrollment_row['id'])
+        true
       end,
-      authorization_requests_sql_where: 'target_api in (\'api_impot_particulier_sandbox\') order by id desc',
-      # authorization_requests_sql_where: 'target_api not in (\'hubee_portail\', \'hubee_portail_dila\', \'api_entreprise\', \'api_particulier\') order by id desc',
+      authorization_requests_sql_where: 'target_api in (\'franceconnect\') order by id',
+      # authorization_requests_sql_where: 'target_api in (\'api_impot_particulier_sandbox\', \'api_impot_particulier_production\') order by case when target_api like \'%_sandbox\' then 1 else 2 end',
       skipped: @skipped,
       warned: @warned,
     }
