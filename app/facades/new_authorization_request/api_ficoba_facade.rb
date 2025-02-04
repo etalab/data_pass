9fc3cc5e746a45fbba12d65210851295217ab9bf
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-02-04 16:50:46 +0100

    Facade to prepare view for select editor form

---
+++ app/facades/new_authorization_request/api_ficoba_facade.rb
@@ -0,0 +1,17 @@
+class NewAuthorizationRequest
+  class APIFicobaFacade < Base
+    def public_available_without_editor_forms
+      AuthorizationRequestFormDecorator.decorate_collection(authorization_definition_sandbox.public_available_forms)
+    end
+
+    def public_available_editor_forms
+      AuthorizationRequestFormDecorator.decorate_collection(authorization_definition.public_available_forms.select { |form| form.uid.ends_with?('editeur') })
+    end
+
+    private
+
+    def authorization_definition_sandbox
+      AuthorizationDefinition.find('api_ficoba_sandbox')
+    end
+  end
+end
