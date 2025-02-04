4387138e1ddf7541e37777f10a6cd621e5d498b2
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-02-04 10:06:58 +0100

    fill the form uid in existing authorizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_04_083243) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_04_090100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "pg_catalog.plpgsql"
