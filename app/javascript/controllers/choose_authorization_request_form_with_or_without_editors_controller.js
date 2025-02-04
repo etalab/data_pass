435bb2f151e5d68bac32b4258f9f3744da6a03a7
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-02-04 14:33:31 +0100

    Fix js lint

--- app/javascript/controllers/choose_authorization_request_form_with_or_without_editors_controller.js
+++ app/javascript/controllers/choose_authorization_request_form_with_or_without_editors_controller.js
@@ -3,13 +3,13 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['withEditorsSection', 'withoutEditorsSection']
   static classes = ['hidden']
-  
+
   toggleEditor (event) {
     const shouldShowEditors = event.currentTarget.value === 'true'
     this.toggleEditorState(shouldShowEditors)
   }
 
-  toggleEditorState(shouldShowEditors) {
+  toggleEditorState (shouldShowEditors) {
     if (shouldShowEditors) {
       this.show(this.withEditorsSectionTarget)
       this.hide(this.withoutEditorsSectionTarget)
