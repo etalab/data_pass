114c64eaf4fbd1e4e9db40918a3cf421ae33ad66
Delmaire Loïc <loic.delmaire@gmail.com>
2025-02-13 07:31:19 +0100

    Introduce Organization#closed? boolean

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -23,4 +23,20 @@ RSpec.describe Organization do
       it { is_expected.to be_nil }
     end
   end
+
+  describe '#closed?' do
+    subject { organization }
+
+    context 'with open etablissement' do
+      let(:organization) { build(:organization, siret: '21920023500014') }
+
+      it { is_expected.not_to be_closed }
+    end
+
+    context 'with closed etablissement' do
+      let(:organization) { build(:organization, siret: '21920023500022') }
+
+      it { is_expected.to be_closed }
+    end
+  end
 end
