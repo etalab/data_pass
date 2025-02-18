7f201b019754be903f206463bb1dd39d3cde30e8
Delmaire Loïc <loic.delmaire@gmail.com>
2025-02-18 17:59:14 +0100

    Introduce FranceConnectMailer#new_scopes

---
+++ spec/mailers/france_connect_mailer_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe FranceConnectMailer, type: :mailer do
+  describe 'new_scopes' do
+    let(:mail) { described_class.with(authorization_request:).new_scopes }
+
+    let(:authorization_request) { create(:authorization_request, :api_droits_cnam, :validated) }
+
+    it 'sends email to FC' do
+      expect(mail.to).to eq(['support.partenaires@franceconnect.gouv.fr'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('FranceConnect associée')
+    end
+  end
+end
