2f2379128de30dce54491ba44ab69159dfafe3fa
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2025-02-19 15:07:33 +0100

    WIP : pb avec le bouton de réouverture et double affichage du footer

--- app/models/authorization_request.rb
+++ app/models/authorization_request.rb
@@ -97,6 +97,10 @@ class AuthorizationRequest < ApplicationRecord
     end
   end
 
+  def latest_authorization_of_stage(stage)
+    authorizations.where(authorization_request_class: stage).order(created_at: :desc).limit(1).first
+  end
+
   def latest_authorization_of_class(authorization_request_class)
     authorizations.where(authorization_request_class: authorization_request_class).order(created_at: :desc).limit(1).first
   end
