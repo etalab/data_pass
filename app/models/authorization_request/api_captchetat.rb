e8c487c358dbc02f138c1533c5002cb4ab7b33d4
Delmaire Loïc <loic.delmaire@gmail.com>
2025-02-19 16:29:08 +0100

    APICaptchEtat: does not require to have cadre juridique

--- app/models/authorization_request/api_captchetat.rb
+++ app/models/authorization_request/api_captchetat.rb
@@ -1,7 +1,12 @@
 class AuthorizationRequest::APICaptchEtat < AuthorizationRequest
   include AuthorizationExtensions::BasicInfos
-  include AuthorizationExtensions::CadreJuridique
   include AuthorizationExtensions::GDPRContacts
+
+  add_attribute :cadre_juridique_nature
+  add_document :cadre_juridique_document, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
+  add_attribute :cadre_juridique_url
+
+  validates :cadre_juridique_url, format: { with: %r{\A((http|https)://)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(/[\w\-._~:\/?#\[\]@!$&'()*+,;%=]*)?\z}, message: I18n.t('activemodel.errors.messages.url_format') }, allow_blank: true, if: -> { need_complete_validation?(:legal) }
 
   add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
 
@@ -9,4 +14,8 @@ class AuthorizationRequest::APICaptchEtat < AuthorizationRequest
     :volumetrie_approximative
 
   contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+
+  def cadre_juridique_document_or_cadre_juridique_url_present
+    nil
+  end
 end
