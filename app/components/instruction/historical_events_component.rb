066d6f17245c4689820c88b86851d04cf2cfa21d
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-03-11 12:22:39 +0100

    Introduce historical_events_component

---
+++ app/components/instruction/historical_events_component.rb
@@ -0,0 +1,106 @@
+class Instruction::HistoricalEventsComponent < ViewComponent::Base
+  attr_reader :authorization_request_event
+
+  def initialize(authorization_request_event:)
+    @authorization_request_event = authorization_request_event
+  end
+
+  def before_render
+    @text_content = case name
+                    when 'request_changes'
+                      helpers.simple_format(authorization_request_event.entity.reason)
+                    when 'applicant_message', 'instructor_message'
+                      helpers.simple_format(authorization_request_event.entity.body)
+                    when 'initial_submit_with_changes_on_prefilled_data', 'submit_with_changes'
+                      humanized_changelog
+                    when 'admin_update'
+                      humanized_changelog(from_admin: true)
+                    end
+  end
+
+  def name
+    case authorization_request_event.name
+    when 'submit'
+      changelog_presenter.event_name
+    when 'cancel_reopening'
+      name_for_cancel_reopening
+    else
+      authorization_request_event.name
+    end
+  end
+
+  def text
+    @text_content
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def transfer_text
+    return unless name == 'transfer'
+
+    if authorization_request_event.entity.from_type == 'User'
+      "#{authorization_request_event.entity.to.full_name} (#{authorization_request_event.entity.to.email})"
+    else
+      "l'organisation #{authorization_request_event.entity.to.raison_sociale} (numéro SIRET : #{authorization_request_event.entity.to.siret})"
+    end
+  end
+
+  # rubocop:enable Metrics/AbcSize
+
+  def message_content
+    @message_content ||= "#{message_summary} #{message_details}".html_safe
+  end
+
+  def message_summary
+    I18n.t(
+      "instruction.authorization_request_events.authorization_request_event.#{name}.message_summary",
+      user_full_name: authorization_request_event.user_full_name,
+      copied_from_authorization_request_id: copied_from_authorization_request_id,
+      transfer_text: transfer_text
+    ).html_safe
+  end
+
+  def message_details
+    return nil if text.blank?
+
+    I18n.t(
+      "instruction.authorization_request_events.authorization_request_event.#{name}.message_details",
+      text: text,
+      copied_from_authorization_request_id: copied_from_authorization_request_id
+    ).html_safe
+  end
+
+  def copied_from_authorization_request_id
+    return unless name == 'copy'
+
+    authorization_request_event.copied_from_request&.id
+  end
+
+  private
+
+  def humanized_changelog(from_admin: false)
+    helpers.content_tag(:ul) do
+      changelog_presenter(from_admin:).consolidated_changelog_entries.map { |entry|
+        helpers.content_tag(:li, entry)
+      }.join.html_safe
+    end
+  end
+
+  def changelog_presenter(from_admin: false)
+    @changelog_presenter ||= AuthorizationRequestChangelogPresenter.new(authorization_request_event.entity, from_admin:)
+  end
+
+  def name_for_cancel_reopening
+    if authorization_request_event.entity.from_instructor?
+      'cancel_reopening_from_instructor'
+    else
+      'cancel_reopening_from_applicant'
+    end
+  end
+
+  delegate :dom_id, :strip_tags, :t, :time_tag, :link_to,
+    :authorization_request_authorization_path,
+    :content_tag,
+    to: :helpers
+  delegate :user_full_name, to: :authorization_request_event
+  delegate :present?, to: :text, prefix: true, allow_nil: true
+end
---
+++ spec/components/instruction/historique_events_component_spec.rb
@@ -0,0 +1,84 @@
+RSpec.describe Instruction::HistoricalEventsComponent, type: :component do
+  subject { described_class.new(authorization_request_event: authorization_request_event) }
+
+  let(:authorization_request_event) { create(:authorization_request_event, :request_changes) }
+
+  before do
+    render_inline(subject)
+  end
+
+  describe '#message_content' do
+    let(:expected_text) { "<strong>Dupont Jean</strong> a demandé des modifications sur la demande :\n <blockquote>\n  <p>Veuillez inclure une preuve de compétence en calligraphie, conformément à la nouvelle directive interne visant à améliorer la qualité visuelle des documents manuscrits officiels</p>\n</blockquote>" }
+
+    it 'calls I18n translation key with the correct variables' do
+      expect(subject.message_content.strip).to eq(expected_text)
+    end
+
+    it 'returns an HTML-safe string' do
+      expect(subject.message_content).to be_html_safe
+    end
+  end
+
+  describe 'render' do
+    let(:expected_text) { "Dupont Jean a demandé des modifications sur la demande :\n\n\n Veuillez inclure une preuve de compétence en calligraphie, conformément à la nouvelle directive interne visant à améliorer la qualité visuelle des documents manuscrits officiels\n\n" }
+
+    it 'renders component' do
+      page = render_inline(subject)
+
+      expect(page).to have_text('Dupont Jean a demandé des modifications sur la demande :')
+      expect(page).to have_text('Veuillez inclure une preuve de compétence en calligraphie, conformément à la nouvelle directive interne visant à améliorer la qualité visuelle des documents manuscrits officiels')
+    end
+  end
+
+  describe '#text_present?' do
+    context 'when text is present' do
+      it 'returns true' do
+        expect(subject.text_present?).to be(true)
+      end
+    end
+
+    context 'when text is nil' do
+      let(:authorization_request_event) { create(:authorization_request_event, :approve) }
+
+      it 'returns false' do
+        render_inline(subject)
+
+        expect(subject.text_present?).to be(false)
+      end
+    end
+  end
+
+  describe '#transfer_text' do
+    let(:to_user) { create(:user, given_name: 'Edmond', family_name: 'Richard', email: 'edmondrichart@example.com') }
+    let(:organization) { create(:organization, siret: '13002526500013') }
+
+    context 'when an authorization_request has been transfer to an user' do
+      let(:authorization_request_event) { create(:authorization_request_event, :transfer) }
+
+      it 'returns the transfer text with the full name and email of the user' do
+        authorization_request_event.entity.to = to_user
+
+        expect(subject.transfer_text).to eq('Richard Edmond (edmondrichart@example.com)')
+      end
+    end
+
+    context 'when an authorization_request has been transfer to an organization' do
+      let(:authorization_request_event) { create(:authorization_request_event, :transfer) }
+
+      it 'returns the transfer text with the raison sociale and SIRET from organization' do
+        authorization_request_event.entity.from_type = organization
+        authorization_request_event.entity.to = organization
+
+        expect(subject.transfer_text).to eq("l'organisation DIRECTION INTERMINISTERIELLE DU NUMERIQUE (numéro SIRET : 13002526500013)")
+      end
+    end
+
+    context 'when event name is submit' do
+      let(:authorization_request_event) { create(:authorization_request_event, :submit) }
+
+      it 'returns nil' do
+        expect(subject.transfer_text).to be_nil
+      end
+    end
+  end
+end
