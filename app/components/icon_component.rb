13393279689762fd6a58283dbe02432910bb3dc1
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-03-13 17:13:18 +0100

    Move spec test icon to new icon component

--- app/components/icon_component.rb
+++ app/components/icon_component.rb
@@ -1,11 +1,4 @@
 class IconComponent < ViewComponent::Base
-  ICONS = {
-            'approve' => { icon: 'checkbox-line', color: 'success' },
-            'request_changes' => { icon: 'pencil-line', color: 'warning' },
-            'refuse' => { icon: 'close-circle-line', color: 'error' },
-            'revoke' => { icon: 'close-circle-line', color: 'error' },
-          }.freeze
-
   def initialize(name:, **options)
     @name = name
     @options = options
@@ -28,8 +21,8 @@ class IconComponent < ViewComponent::Base
   end
 
   def icon_class
-    event = available_icons[@name] || (raise 'Invalid icon')
+    name = available_icons[@name] || (raise 'Invalid icon')
 
-    %W[fr-icon-#{event[:icon]} fr-text-#{event[:color]}].join(' ')
+    %W[fr-icon-#{name[:icon]} fr-text-#{name[:color]}].join(' ')
   end
 end
