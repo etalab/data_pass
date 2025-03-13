1959aba1383c7beaae04c83b79dc162f0681571e
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-03-13 17:13:18 +0100

    Move spec test icon to new icon component

--- spec/components/historical_authorization_request_event_icon_component_spec.rb
+++ spec/components/historical_authorization_request_event_icon_component_spec.rb
@@ -1,15 +1,44 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
 RSpec.describe HistoricalAuthorizationRequestEventIconComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#call' do
+    context "when event_name is 'approve'" do
+      it 'renders the correct icon and color class' do
+        render_inline(described_class.new(name: 'approve'))
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+        expect(page).to have_css("i.fr-icon-checkbox-line.fr-text-success[aria-hidden='true']")
+      end
+    end
+
+    context "when event_name is 'request_changes'" do
+      it 'renders the correct icon and color class' do
+        render_inline(described_class.new(name: 'request_changes'))
+
+        expect(page).to have_css("i.fr-icon-pencil-line.fr-text-warning[aria-hidden='true']")
+      end
+    end
+
+    context "when event_name is 'refuse'" do
+      it 'renders the correct icon and color class' do
+        render_inline(described_class.new(name: 'refuse'))
+
+        expect(page).to have_css("i.fr-icon-close-circle-line.fr-text-error[aria-hidden='true']")
+      end
+    end
+
+    context "when event_name is 'revoke'" do
+      it "renders the same icon and color class as 'refuse'" do
+        render_inline(described_class.new(name: 'revoke'))
+
+        expect(page).to have_css("i.fr-icon-close-circle-line.fr-text-error[aria-hidden='true']")
+      end
+    end
+
+    context 'when event_name is unknown' do
+      it 'renders the default icon and color' do
+        render_inline(described_class.new(name: 'unknown_event'))
+
+        expect(page).to have_css("i.fr-icon-error-warning-line.fr-text-info[aria-hidden='true']")
+      end
+    end
+  end
 end
+
