f2999ad37a198233c1c46ea79544a03ab2c828ce
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-03-17 15:00:34 +0100

    scout commit : rubocop

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,7 +1,6 @@
 class ApplicationComponent < ViewComponent::Base
   delegate :dom_id, :strip_tags, :t, :link_to,
-           :authorization_request_authorization_path,
-           :content_tag,
-           to: :helpers
-
+    :authorization_request_authorization_path,
+    :content_tag,
+    to: :helpers
 end
