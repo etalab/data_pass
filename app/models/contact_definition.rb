b016e936d77948250c0e8cc00c315fc52d141364
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-03-17 16:58:08 +0100

    Create method in contact_definition model and call it into view to display only it's me button for specific types of contact
      - contact_metier / administrateur_metier / contact_technique only

--- app/models/contact_definition.rb
+++ app/models/contact_definition.rb
@@ -1,6 +1,8 @@
 class ContactDefinition
   attr_reader :type, :options
 
+  FILLABLE_WITH_APPLICANT_DATA_TYPES = %i[contact_technique contact_metier administrateur_metier].freeze
+
   def initialize(type, options = {})
     @type = type
     @options = options
@@ -8,5 +10,9 @@ class ContactDefinition
 
   def required_personal_email?
     options.fetch(:required_personal_email, false)
+  end
+
+  def fill_data_with_applicant_data?
+    type.in?(FILLABLE_WITH_APPLICANT_DATA_TYPES)
   end
 end
