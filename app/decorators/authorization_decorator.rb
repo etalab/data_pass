15c28fa7aab7502aeb582d912c659e90180f713a
Delmaire Loïc <loic.delmaire@gmail.com>
2025-03-17 17:48:08 +0100

    Change AuthorizationDecorator#name_for_select to use proper date instead of strange slug

--- app/decorators/authorization_decorator.rb
+++ app/decorators/authorization_decorator.rb
@@ -2,6 +2,12 @@ class AuthorizationDecorator < ApplicationDecorator
   delegate_all
 
   def name_for_select
-    "Habilitation du #{slug} : #{name}"
+    "Habilitation du #{formatted_date} : #{name}"
+  end
+
+  private
+
+  def formatted_date
+    I18n.l(created_at.to_date, format: :long)
   end
 end
