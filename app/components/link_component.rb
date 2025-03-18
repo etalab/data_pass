b99394c5c15988c52aa55b14d916f20c734b45f5
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-03-18 16:12:04 +0100

    Review: Improve LinkComponent

--- app/components/link_component.rb
+++ app/components/link_component.rb
@@ -1,16 +1,14 @@
 class LinkComponent < ApplicationComponent
-  def initialize(text: nil, path: nil, i18n_key: nil, **options)
+  def initialize(text:, path:, **options)
     @text = text
     @path = path
-    @i18n_key = i18n_key
     @options = merge_options(options)
   end
 
   def call
-    return if @path.blank?
 
     link_to(
-      link_text,
+      @text,
       @path,
       **@options
     )
@@ -18,13 +16,8 @@ class LinkComponent < ApplicationComponent
 
   private
 
-  def link_text
-    @text || (@i18n_key.present? ? t(@i18n_key) : '')
-  end
-
   def default_options
     {
-      target: '_blank',
       class: 'fr-link',
       rel: 'noopener'
     }
@@ -32,7 +25,7 @@ class LinkComponent < ApplicationComponent
 
   def merge_options(custom_options)
     merged_options = default_options.merge(custom_options.except(:class))
-    merged_options[:class] = [default_options[:class], custom_options[:class]].compact.join(' ')
+    merged_options[:class] = [default_options[:class], custom_options[:class]].flatten
     merged_options
   end
 end
--- spec/components/link_component_spec.rb
+++ spec/components/link_component_spec.rb
@@ -3,91 +3,58 @@ RSpec.describe LinkComponent, type: :component do
     let(:path) { 'https://example.com' }
     let(:text) { 'Visiter le site' }
 
-    context 'when path is present' do
-      before do
-        fr = {
-          dashboard: {
-            lien: 'Visiter le site'
-          }
+    before do
+      fr = {
+        dashboard: {
+          lien: 'Visiter le site'
         }
+      }
 
-        I18n.backend.store_translations(:fr, fr)
-      end
-
-      it 'renders link with text' do
-        component = render_inline(described_class.new(
-          text: text,
-          path: path
-        ))
-
-        expect(component).to have_link('Visiter le site', href: 'https://example.com')
-        expect(component).to have_css("a.fr-link[target='_blank'][rel='noopener']")
-      end
-
-      it 'renders link with the translation of the provided i18n key' do
-        component = I18n.with_locale(:fr) do
-          render_inline(described_class.new(
-            i18n_key: 'dashboard.lien',
-            path: path
-          ))
-        end
-
-        expect(component).to have_link('Visiter le site', href: 'https://example.com')
-      end
-
-      it 'applies custom options while preserving default values' do
-        component = render_inline(described_class.new(
-          text: text,
-          path: path,
-          class: 'my-other-class'
-        ))
-
-        puts component.css('a').to_html
-
-        expect(component).to have_css('a.fr-link.my-other-class')
-        expect(component).to have_css("a[target='_blank'][rel='noopener']")
-      end
-
-      it 'replaces default options with those provided' do
-        component = render_inline(described_class.new(
-          text: 'Autre target',
-          path: path,
-          target: '_self'
-        ))
-
-        expect(component).to have_css("a[target='_self']")
-      end
+      I18n.backend.store_translations(:fr, fr)
     end
 
-    context 'when path is missing or nil' do
-      it 'does not render link when path is missing' do
-        component = render_inline(described_class.new(text: 'Texte'))
-        expect(component.css('a')).to be_empty
-      end
+    it 'renders link with text' do
+      component = render_inline(described_class.new(
+        text: text,
+        path: path
+      ))
 
-      it 'does not render link when path is empty' do
-        component = render_inline(described_class.new(text: 'Texte', path: ''))
-        expect(component.css('a')).to be_empty
-      end
+      expect(component).to have_link('Visiter le site', href: 'https://example.com')
+      expect(component).to have_css("a.fr-link[rel='noopener']")
+      expect(component).not_to have_css("a[target='_blank']")
     end
 
-    context 'when there is text priority' do
-      it 'renders direct text rather than the i18n key if both are provided' do
-        component = render_inline(described_class.new(
-          text: text,
-          i18n_key: 'key.that.should.not.be.present',
-          path: path
-        ))
+    it 'applies custom options while preserving default values' do
+      component = render_inline(described_class.new(
+        text: text,
+        path: path,
+        class: 'my-other-class',
+        target: '_blank'
 
-        expect(component).to have_link('Visiter le site')
-      end
+      ))
 
-      it 'renders an empty link if neither text of i18n key are provided' do
-        component = render_inline(described_class.new(path: path))
+      expect(component).to have_css('a.fr-link.my-other-class')
+      expect(component).to have_css("a[target='_blank'][rel='noopener']")
+    end
 
-        expect(component).to have_link(href: 'https://example.com')
-        expect(component.text).to be_blank
-      end
+    it 'replaces default options with those provided' do
+      component = render_inline(described_class.new(
+        text: 'Autre target',
+        path: path,
+        target: '_self'
+      ))
+
+      expect(component).to have_css("a[target='_self']")
+    end
+
+    it 'accepts translated text as input' do
+      translated_text = I18n.t('dashboard.lien')
+      component = render_inline(described_class.new(
+        text: translated_text,
+        path: path
+      ))
+
+      expect(component).to have_link('Visiter le site', href: 'https://example.com')
     end
   end
 end
