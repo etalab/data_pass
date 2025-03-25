8ec805a25a11a041a32effcae1add9bad12010b3
Delmaire Loïc <loic.delmaire@gmail.com>
2025-03-25 17:53:53 +0100

    Can specific disabled scopes on AuthorizationDefinition

--- app/models/authorization_definition/scope.rb
+++ app/models/authorization_definition/scope.rb
@@ -2,8 +2,7 @@ class AuthorizationDefinition::Scope
   attr_reader :name,
     :value,
     :group,
-    :link,
-    :included
+    :link
 
   def initialize(properties)
     @name = properties.fetch(:name)
@@ -11,10 +10,15 @@ class AuthorizationDefinition::Scope
     @group = properties.fetch(:group, nil)
     @link = properties.fetch(:link, nil)
     @included = properties.fetch(:included, false)
+    @disabled = properties.fetch(:disabled, false)
   end
 
   def included?
-    included
+    @included
+  end
+
+  def disabled?
+    @disabled
   end
 
   def link?
