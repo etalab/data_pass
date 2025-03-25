2af1c0be78d978404776a6cc9c74689f7a22bdb0
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2025-03-25 18:47:51 +0100

    Marque les habilitations comme révoquées

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -55,6 +55,20 @@ class Authorization < ApplicationRecord
 
     event :revoke do
       transition from: :active, to: :revoked
+      transition from: :obsolete, to: :obsolete
+    end
+
+    event :rollback_revoke do
+      transition from: :revoked, to: :active
+      transition from: :obsolete, to: :obsolete
+    end
+
+    after_transition on: :revoke do |authorization, _transition|
+      authorization.update(revoked: true)
+    end
+
+    after_transition on: :rollback_revoke do |authorization, _transition|
+      authorization.update(revoked: false)
     end
   end
 
