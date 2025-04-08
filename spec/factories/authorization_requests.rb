d1bc7b59a29db1bb7599986408889af548a932a4
Delmaire Loïc <loic.delmaire@gmail.com>
2025-04-08 12:41:41 +0100

    Harden API INFINOE
    1. Remove safety_certification block: does not exists in v1
    2. Tests all forms

--- spec/factories/authorization_requests.rb
+++ spec/factories/authorization_requests.rb
@@ -1139,7 +1139,6 @@ FactoryBot.define do
       with_basic_infos
       with_personal_data
       with_cadre_juridique
-      with_safety_certification
       with_operational_acceptance
       with_volumetrie
     end
