53dfc1293045b338fb4aab5750221f763ead1c78
Delmaire Loïc <loic.delmaire@gmail.com>
2025-04-10 17:30:49 +0100

    Migration: harden local run

--- app/migration/local_run.sh
+++ app/migration/local_run.sh
@@ -3,6 +3,7 @@
 bundle exec rails db:drop db:create || exit 1
 bundle exec rails db:environment:set RAILS_ENV=development
 pg_restore -h localhost -d development app/migration/dumps/datapass_production_v2.dump 2> /dev/null
+bundle exec rails db:migrate
 LOCAL=true bundle exec rails runner "ImportDataInLocalDb.new.perform(delete_db_file: false)"
 
 if [ $# -eq 1 ]; then
