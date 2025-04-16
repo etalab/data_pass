c7fee534e8564334732b865bbb1236ec04b18953
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-04-16 11:59:18 +0200

    new dump and instructor roles

--- lib/suivi_dtnum/import_sql_dump_in_docker.sh
+++ lib/suivi_dtnum/import_sql_dump_in_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+scp watchdoge:dump-all-2025-04-16.sql ./lib/suivi_dtnum/sources/
 
 # This script imports a SQL dump into the development database in Docker
 # It first drops all existing tables and then imports the dump
@@ -7,7 +8,7 @@
 docker compose exec db psql -U postgres -d development -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 
 # Copy the SQL dump to the container and import it
-docker compose cp "lib/suivi DTNUM/sources/dump-all-2025-04-15.sql" db:/tmp/dump.sql
+docker compose cp "lib/suivi_dtnum/sources/dump-all-2025-04-16.sql" db:/tmp/dump.sql
 docker compose exec db psql -U postgres -d development -f /tmp/dump.sql
 
 # Note: You might see errors about role "skelz0r" not existing, but these can be safely ignored
