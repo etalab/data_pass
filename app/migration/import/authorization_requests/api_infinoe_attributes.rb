220a91f229d9f67712c125bfc06389720deabf27
Delmaire Loïc <loic.delmaire@gmail.com>
2025-04-16 11:52:11 +0100

    Migration: harden API INFINOE

--- app/migration/import/authorization_requests/api_infinoe_attributes.rb
+++ app/migration/import/authorization_requests/api_infinoe_attributes.rb
@@ -1,2 +1,20 @@
 class Import::AuthorizationRequests::APIINFINOEAttributes < Import::AuthorizationRequests::APIRialAttributes
+  def affect_data
+    super
+    affect_form_uid
+  end
+
+  def demarche_to_form_uid
+    if enrollment_row['target_api'] =~ /_unique$/
+      'api-infinoe-envoi-automatise-ecritures-production-editeur'
+    else
+      case enrollment_row['demarche']
+      when 'envoi_ecritures'
+        'api-infinoe-envoi-automatise-ecritures-production'
+      else
+        'api-infinoe-production'
+      end
+    end
+  end
+
 end
