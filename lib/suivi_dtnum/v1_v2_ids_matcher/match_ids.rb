991e78276b1778b81f8f8c558328f015181c627d
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-04-18 11:29:42 +0200

    improve id matcher a lot

--- lib/suivi_dtnum/v1_v2_ids_matcher/match_ids.rb
+++ lib/suivi_dtnum/v1_v2_ids_matcher/match_ids.rb
@@ -19,59 +19,81 @@ matching_requests.each do |request|
 end
 
 # Create the result data as an array first
-results = []
 matched_ids = []
+extra_authorizations_ids = []
+row_with_no_authorization_id_equal_to_v1_id = []
 
 v1_ids.each do |v1_id|
   if v1_to_v2_map.key?(v1_id)
     request, authorizations = v1_to_v2_map[v1_id]
-    
+
+    # Create a row for each authorization
     if authorizations.any?
-      # Create a row for each authorization
-      # Actually we do it only when the authorization has the same id as the v1_id, cause the others should not exit
-      # (it's getting fixed in the next dump)
-      authorizations_with_matching_id = authorizations.where(id: v1_id)
-      
-      if authorizations_with_matching_id.any?
-        authorizations_with_matching_id.each do |authorization|
-          results << [v1_id, request.id, authorization.id]
-          matched_ids << v1_id
+      authorizations.each do |authorization|
+        # keep the authorization with the same id as the v1_id
+        if authorization.id == v1_id.to_i
+          matched_ids << [v1_id, request.id, authorization.id]
+        # log the others as extra, they should not exist
+        else
+          extra_authorizations_ids << [v1_id, request.id, authorization.id]
         end
-      else
-        # If no matching authorizations, create a row with empty habilitation_v2_id
-        results << [v1_id, request.id, nil]
-        matched_ids << v1_id
+
+        # If no authorization was found with the same id as the v1_id, create a row with empty habilitation_v2_id to not skip a row
+        unless authorizations.map(&:id).include? v1_id.to_i
+          matched_ids << [v1_id, request.id, nil]
+          authorizations.each do |authorization|
+            row_with_no_authorization_id_equal_to_v1_id << [v1_id, request.id, authorization.id]
+          end
+        end
       end
     else
       # If no authorizations, create a row with empty habilitation_v2_id
-      results << [v1_id, request.id, nil]
-      matched_ids << v1_id
+      matched_ids << [v1_id, request.id, nil]
     end
   else
     # If no matching request found, create a row with only the v1_id
-    results << [v1_id, nil, nil]
-    matched_ids << v1_id
+    matched_ids << [v1_id, nil, nil]
   end
 end
 
 # Check for missing v1_ids
 if matched_ids.length != v1_ids.length
-  puts "Warning: Not all v1_ids were processed"
+  puts "\nWarning: Not all v1_ids were processed"
   puts "v1_ids.length: #{v1_ids.length}"
   puts "matched_ids.uniq.length: #{matched_ids.length}"
 
   # Find the missing v1_ids
-  missing_v1_ids = v1_ids - matched_ids.uniq
-  puts "Missing v1_ids: #{missing_v1_ids.join(', ')}"
+  missing_v1_ids = v1_ids - matched_ids.uniq.map { |row| row[0] }
+  puts "Missing v1_ids: #{missing_v1_ids.join(', ')}\n"
 end
+
+if row_with_no_authorization_id_equal_to_v1_id.any?
+  puts "\nFound #{row_with_no_authorization_id_equal_to_v1_id.length} rows with no authorization id equal to v1_id:"
+  row_with_no_authorization_id_equal_to_v1_id.each do |row|
+    puts "v1_id: #{row[0]}, demande_v2_id: #{row[1]}, habilitation_v2_id: #{row[2]}"
+  end
+  puts "\n"
+end
+
+
+if extra_authorizations_ids.any?
+  puts "Found #{extra_authorizations_ids.length} extra authorizations ids -> see extra_authorizations_ids.csv"
+
+  CSV.open('lib/suivi_dtnum/v1_v2_ids_matcher/extra_authorizations_ids.csv', 'w') do |csv|
+    csv << ['datapass_v1_id', 'demande_v2_id', 'habilitation_v2_id']
+    extra_authorizations_ids.each do |row|
+      csv << row
+    end
+  end
+end
+
 
 # Output results to CSV
 CSV.open('lib/suivi_dtnum/v1_v2_ids_matcher/matched_ids.csv', 'w') do |csv|
   csv << ['datapass_v1_id', 'demande_v2_id', 'habilitation_v2_id']
-  
-  results.each do |row|
+
+  matched_ids.each do |row|
     csv << row
   end
 end
-
 puts "Results written to matched_ids.csv\n\n"
