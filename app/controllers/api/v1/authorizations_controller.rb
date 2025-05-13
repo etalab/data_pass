0e48c92d514ca358a42a1fd9d761b0c87090130c
Delmaire Loïc <loic.delmaire@gmail.com>
2025-05-13 18:09:56 +0200

    API: enforce read_authorization_requests scope on endpoints, make it default

--- app/controllers/api/v1/authorizations_controller.rb
+++ app/controllers/api/v1/authorizations_controller.rb
@@ -1,5 +1,6 @@
 class API::V1::AuthorizationsController < API::V1Controller
   before_action :set_authorization_request
+  before_action -> { doorkeeper_authorize! :read_authorization_requests }, only: :index
 
   def index
     render json: @authorization_request.authorizations,
