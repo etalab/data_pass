c1872bd093936e9dd95520ca7ac7e452bd78187e
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-05-14 17:11:12 +0200

    Implement a Stimulus controller to remove attached files from demandes
      - Add a trash icon button next to the file name

---
+++ app/javascript/controllers/remove_attached_file_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  removeFile(event) {
+    const button = event.currentTarget
+    const fieldId = button.dataset.fieldId
+
+    const hiddenField = document.getElementById(fieldId)
+
+    if (hiddenField) {
+      hiddenField.remove()
+
+      const fileDisplay = button.closest('.file-with-remove-button')
+      if (fileDisplay) {
+        fileDisplay.remove()
+      }
+    }
+  }
+}
