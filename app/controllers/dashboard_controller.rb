14ef9f90fcaff5089d076324a2e8514f0518fd88
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2025-05-28 16:26:29 +0200

    Allay, on a des bons tabs, il faut intégrer les changements sur les cards

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,49 @@
 class DashboardController < AuthenticatedUserController
   include SubdomainsHelper
 
+  decorates_assigned :authorization_requests
+
   def index
-    redirect_to dashboard_show_path(id: 'moi')
+    redirect_to dashboard_show_path(id: 'demandes')
   end
 
   def show
+    @tabs = [
+      Tab.new('demandes', dashboard_show_path(id: 'demandes', **request.query_parameters)),
+      Tab.new('habilitations', dashboard_show_path(id: 'habilitations', **request.query_parameters)),
+    ]
+
     case params[:id]
+    when 'demandes'
+      @items = policy_scope(base_relation).not_archived.order(created_at: :desc)
+      @highlighted_categories = {
+        changes_requested: @items.changes_requested.without_reopening,
+      }
+      @categories = { 
+        pending: @items.in_instructions.without_reopening,
+        draft: @items.drafts.without_reopening,
+        validated_or_refused: @items.validated_or_refused.without_reopening,
+      }
+    when 'habilitations'
+      @items = current_user.authorizations_as_applicant.order(created_at: :desc)
+      @highlighted_categories = {}
+      @categories = { 
+        active: @items.where(state: :active),
+        revoked: @items.where(state: :revoked),
+      }
+    end
+  end
+
+  private
+
+  class Tab < Data.define(:id, :path)
+  end
+
+  def dashboard_v1
+    case params[:id]
+    when 'demandes'
+      @items = policy_scope(AuthorizationRequest)
+      @authorization_requests = AuthorizationRequest.none
     when 'moi'
       @authorization_requests = policy_scope(base_relation).where(applicant: current_user)
     when 'organisation'
@@ -19,9 +56,9 @@ class DashboardController < AuthenticatedUserController
     end
 
     @authorization_requests = @authorization_requests.not_archived.order(created_at: :desc)
+    
+    # render :show_v1, layout: 'dashboard'
   end
-
-  private
 
   def authorization_requests_relation
     if registered_subdomain?
@@ -38,7 +75,7 @@ class DashboardController < AuthenticatedUserController
       .group('authorization_requests.id')
   end
 
-  def layout_name
-    'dashboard'
-  end
+  # def layout_name
+  #   'dashboard'
+  # end
 end
--- app/decorators/authorization_decorator.rb
+++ app/decorators/authorization_decorator.rb
@@ -3,6 +3,29 @@ class AuthorizationDecorator < ApplicationDecorator
 
   decorates_association :request
 
+  def card_name
+    base_card_name.truncate(50)
+  end
+
+  def card_provider_applicant_details(user)
+    if only_in_contacts?(user)
+      current_user_is_a_contact(user)
+    elsif object.applicant == user
+      current_user_is_applicant
+    else
+      default_applicant_full_name
+    end
+  end
+  def only_in_contacts?(user)
+    user != object.applicant &&
+      object.contact_types_for(user).present?
+  end
+  def reopening_validated?
+    object.request.authorizations.where(authorization_request_class: object.authorization_request_class).count > 1
+  rescue NoMethodError
+    false
+  end
+
   def name_for_select
     "Habilitation n°#{id} du #{formatted_date} : #{name}"
   end
@@ -12,6 +35,31 @@ class AuthorizationDecorator < ApplicationDecorator
   end
 
   private
+
+  # rubocop:disable Metrics/AbcSize
+  def base_card_name
+    if object.data['intitule'].present?
+      object.data['intitule']
+    elsif object.request.form.name.present?
+      object.request.form.name
+    else
+      object.definition.name
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def current_user_is_a_contact(user)
+    t('dashboard.card.authorization_request_card.current_user_mentions', definition_name: object.definition.name, contact_types: humanized_contact_types_for(user).to_sentence)
+  end
+
+  def current_user_is_applicant
+    t('dashboard.card.authorization_request_card.current_user_is_applicant', definition_name: object.definition.name)
+  end
+
+  def default_applicant_full_name
+    t('dashboard.card.authorization_request_card.applicant_request', definition_name: object.definition.name, applicant_full_name: object.applicant.full_name)
+  end
+
 
   def formatted_date
     I18n.l(created_at.to_date, format: :long)
