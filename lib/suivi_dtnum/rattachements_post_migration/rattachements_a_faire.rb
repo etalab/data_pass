88dbc2bc63813086616e5c49caa554d2d9d6fb8e
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-06-04 12:46:48 +0200

    make groups

--- lib/suivi_dtnum/rattachements_post_migration/rattachements_a_faire.rb
+++ lib/suivi_dtnum/rattachements_post_migration/rattachements_a_faire.rb
@@ -1,19 +1,52 @@
 require 'csv'
 
-# Open and read the CSV file
-csv_file_path = File.join(__dir__, 'ids_fichier_de_suivi.csv')
+class RattachementsAFaire
+  def initialize
+    @csv_file_path = File.join(__dir__, 'ids_fichier_de_suivi.csv')
+  end
 
-begin
-  # Read the CSV file with headers
-  csv_data = CSV.read(csv_file_path, headers: true)
+  def run
+    read_csv
+    identify_rattachements_to_make
 
-  puts "Successfully loaded CSV file: #{csv_file_path}"
+    p @groups.values.map(&:count).group_by(&:itself).map { |k, v| [k, v.count] }
+  end
 
+  def read_csv
+    @csv = CSV.read(@csv_file_path, headers: true)
+  end
 
-rescue CSV::MalformedCSVError => e
-  puts "Error: Malformed CSV file - #{e.message}"
-rescue Errno::ENOENT
-  puts "Error: CSV file not found at #{csv_file_path}"
-rescue => e
-  puts "Error reading CSV file: #{e.message}"
+  def identify_rattachements_to_make
+    @groups = {}
+
+    @csv.each do |row|
+      group = find_complete_group(row)
+      most_recent = most_recent_of_group(group)
+      add_to_group(most_recent["N° Demande v2"], row)
+    end
+
+    @groups.select! { |id, group| group.count > 1 && id != nil }
+  end
+
+  def add_to_group(group_id, row)
+    @groups[group_id] ||= []
+    @groups[group_id] << row.to_h
+  end
+
+  def find_complete_group(row, group=[])
+    group << row
+    if row['N° DataPass copié']
+      find_complete_group(row['N° DataPass copié'], group)
+    else
+      group
+    end
+  end
+
+  def most_recent_of_group(group)
+    group.find do |row|
+      group.none? { |r| r["N° DataPass copié"] == row["N° DataPass v1"]}
+    end
+  end
 end
+
+RattachementsAFaire.new.run
