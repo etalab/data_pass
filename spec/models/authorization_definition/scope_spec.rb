b3f8d646faf3194f5f56f4244c064c1de14d608b
Delmaire Loïc <loic.delmaire@gmail.com>
2025-06-05 09:03:47 +0200

    Refactor scope management
    Move complex logic within PORO

--- spec/models/authorization_definition/scope_spec.rb
+++ spec/models/authorization_definition/scope_spec.rb
@@ -106,4 +106,126 @@ RSpec.describe AuthorizationDefinition::Scope do
       it { is_expected.to be(false) }
     end
   end
+
+  describe '#available?' do
+    subject { scope.available?(request) }
+
+    let(:request) { instance_double(AuthorizationRequest, created_at: created_at, form: form) }
+    let(:form) { instance_double(AuthorizationRequestForm, scopes_config: scopes_config) }
+    let(:created_at) { Time.zone.now }
+    let(:scopes_config) { {} }
+
+    context 'when scope is not deprecated' do
+      let(:scope) do
+        described_class.new(
+          name: 'Test Scope',
+          value: 'test_scope'
+        )
+      end
+
+      context 'when scopes_config[:displayed] is blank' do
+        let(:scopes_config) { { displayed: nil } }
+
+        it { is_expected.to be true }
+      end
+
+      context 'when scopes_config[:displayed] is empty array' do
+        let(:scopes_config) { { displayed: [] } }
+
+        it { is_expected.to be true }
+      end
+
+      context 'when scopes_config[:displayed] is not configured' do
+        let(:scopes_config) { {} }
+
+        it { is_expected.to be true }
+      end
+
+      context 'when scopes_config[:displayed] contains the scope value' do
+        let(:scopes_config) { { displayed: %w[test_scope other_scope] } }
+
+        it { is_expected.to be true }
+      end
+
+      context 'when scopes_config[:displayed] does not contain the scope value' do
+        let(:scopes_config) { { displayed: %w[other_scope] } }
+
+        it { is_expected.to be false }
+      end
+    end
+
+    context 'when scope is deprecated' do
+      let(:deprecated_date) { '2025-04-17' }
+      let(:scope) do
+        described_class.new(
+          name: 'Test Scope',
+          value: 'test_scope',
+          deprecated_since: deprecated_date
+        )
+      end
+
+      context 'when entity was created before deprecation date' do
+        let(:created_at) { Date.parse('2025-04-16').to_time }
+
+        context 'when scopes_config[:displayed] is blank' do
+          let(:scopes_config) { { displayed: nil } }
+
+          it { is_expected.to be true }
+        end
+
+        context 'when scopes_config[:displayed] contains the scope value' do
+          let(:scopes_config) { { displayed: %w[test_scope] } }
+
+          it { is_expected.to be true }
+        end
+
+        context 'when scopes_config[:displayed] does not contain the scope value' do
+          let(:scopes_config) { { displayed: %w[other_scope] } }
+
+          it { is_expected.to be false }
+        end
+      end
+
+      context 'when entity was created after deprecation date' do
+        let(:created_at) { Date.parse('2025-04-18').to_time }
+
+        context 'when scopes_config is configured' do
+          let(:scopes_config) { { displayed: %w[test_scope] } }
+
+          it 'returns false due to deprecation' do
+            expect(subject).to be false
+          end
+        end
+      end
+
+      context 'when entity has no created_at date' do
+        let(:request) { instance_double(AuthorizationRequest, created_at: nil, form: form) }
+
+        context 'when today is before deprecation date' do
+          it 'returns true if scope is in displayed list' do
+            travel_to Date.parse('2025-04-16') do
+              allow(form).to receive(:scopes_config).and_return({ displayed: %w[test_scope] })
+              expect(subject).to be true
+            end
+          end
+
+          it 'returns true if displayed list is blank' do
+            travel_to Date.parse('2025-04-16') do
+              allow(form).to receive(:scopes_config).and_return({ displayed: nil })
+              expect(subject).to be true
+            end
+          end
+        end
+
+        context 'when today is after deprecation date' do
+          it 'returns false regardless of scopes_config' do
+            travel_to Date.parse('2025-04-18') do
+              allow(form).to receive(:scopes_config).and_return({ displayed: %w[test_scope] })
+              expect(subject).to be false
+            end
+          end
+        end
+      end
+    end
+  end
 end
