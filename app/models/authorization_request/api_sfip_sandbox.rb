3bfe720d02d082475ecda50fde9d78fc2d711917
Nicolas Schweisguth <nicolas.schweisguth@beta.gouv.fr>
2025-06-12 17:41:21 +0200

    Add volumétrie approximative pour IP et SFiP

--- app/models/authorization_request/api_sfip_sandbox.rb
+++ app/models/authorization_request/api_sfip_sandbox.rb
@@ -8,7 +8,8 @@ class AuthorizationRequest::APISFiPSandbox < AuthorizationRequest
 
   add_document :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
 
-  add_attributes :date_prevue_mise_en_production
+  add_attributes :date_prevue_mise_en_production,
+    :volumetrie_approximative
 
   add_scopes(validation: {
     presence: true, if: -> { need_complete_validation?(:scopes) }
