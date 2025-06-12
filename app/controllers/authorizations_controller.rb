ce4518a7a1b16df3ed65738c10a764b9c8d98e43
Delmaire Loïc <loic.delmaire@gmail.com>
2025-06-12 17:02:11 +0200

    Refactor form_builder built within AuthorizationsController#show

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -6,14 +6,6 @@ class AuthorizationsController < AuthenticatedUserController
 
   def show
     authorize @authorization, :show?
-
-    # @authorization_request = @authorization.request_as_validated.decorate
-    @form_builder = AuthorizationRequestFormBuilder.new(
-      'authorization_request',
-      @authorization.request_as_validated,
-      view_context,
-      {}
-    )
   end
 
   private
--- app/helpers/demandes_habilitations/common_helper.rb
+++ app/helpers/demandes_habilitations/common_helper.rb
@@ -1,7 +1,20 @@
 module DemandesHabilitations::CommonHelper
   def render_custom_block_or_default(authorization_request, block_id, locals = {})
+    locals[:f] ||= build_form_builder(authorization_request)
+
     render partial: "authorization_requests/blocks/#{authorization_request.definition.id}/#{block_id}", locals: { authorization_request:, **locals }
   rescue ActionView::MissingTemplate
     render partial: "authorization_requests/blocks/default/#{block_id}", locals: { authorization_request:, **locals }
+  end
+
+  private
+
+  def build_form_builder(authorization_request)
+    AuthorizationRequestFormBuilder.new(
+      'authorization_request',
+      authorization_request,
+      self,
+      {}
+    )
   end
 end
