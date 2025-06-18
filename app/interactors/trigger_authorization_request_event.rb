cc2b14331f424b1bf82c26f43559629ba2fe3e89
Delmaire Loïc <loic.delmaire@gmail.com>
2025-06-18 13:38:35 +0200

    TriggerAuthorizationRequestEvent can bypass state machine
    We have some authorization request which are not valid (because of dirty
    data from v1), so we can't trigger state machine events for some
    features, so we had to make TriggerAuthorizationRequestEvent more
    flexible in order to use
    ExecuteAuthorizationRequestTransitionWithCallbacks.
    
    Will be used in future commits

--- app/interactors/trigger_authorization_request_event.rb
+++ app/interactors/trigger_authorization_request_event.rb
@@ -1,11 +1,28 @@
 class TriggerAuthorizationRequestEvent < ApplicationInteractor
   def call
-    return if trigger_event
+    return if trigger_or_event_valid
 
     context.fail!(error: :trigger_event_failed)
   end
 
   private
+
+  def trigger_or_event_valid
+    if context.bypass_state_machine_event
+      manually_affect_state
+    else
+      trigger_event
+    end
+  end
+
+  def manually_affect_state
+    if context.authorization_request.public_send("can_#{context.state_machine_event}?")
+      context.authorization_request.state = context.state_machine_new_state
+      true
+    else
+      false
+    end
+  end
 
   def trigger_event
     context.authorization_request.public_send(context.state_machine_event)
