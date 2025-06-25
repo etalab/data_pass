5ede342627d8e3e56aaa0d713786da2768f83364
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2025-06-25 12:26:44 +0200

    Linting

--- app/javascript/controllers/clickable_rows_controller.js
+++ app/javascript/controllers/clickable_rows_controller.js
@@ -2,8 +2,8 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   visit (event) {
-    var selection = window.getSelection();
-    if (selection.type != "Range") {
+    const selection = window.getSelection()
+    if (selection.type !== 'Range') {
       const row = event.currentTarget
       const path = row.attributes['data-href'].value
       window.location.assign(path)
