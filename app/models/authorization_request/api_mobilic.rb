afb683414828fc5e1afef797d7d35c43c63670bb
Delmaire Loïc <loic.delmaire@gmail.com>
2025-07-08 14:43:42 +0200

    API Mobilic: validates phone numbers
    Closes https://linear.app/pole-api/issue/DAT-1174

--- app/models/authorization_request/api_mobilic.rb
+++ app/models/authorization_request/api_mobilic.rb
@@ -8,4 +8,12 @@ class AuthorizationRequest::APIMobilic < AuthorizationRequest
   validates :date_prevue_mise_en_production, presence: true, if: -> { need_complete_validation?(:basic_infos) }
 
   contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+
+  %i[
+    contact_technique
+    responsable_traitement
+    delegue_protection_donnees
+  ].each do |contact_kind|
+    validates "#{contact_kind}_phone_number", french_phone_number: true, if: ->(record) { record.need_complete_validation?(:contacts) }
+  end
 end
