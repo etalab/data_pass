451ab9848dd6310b9cb975cad9da7ecd39dfc1a6
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-07-23 16:13:54 +0200

    Create a Dashboard Facade

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,37 +1,21 @@
 class DashboardController < AuthenticatedUserController
   include SubdomainsHelper
 
+  Tab = Data.define(:id, :path)
+
   def index
     redirect_to dashboard_show_path(id: 'demandes')
   end
 
-  Tab = Data.define(:id, :path)
-
-  def show # rubocop:disable Metrics/AbcSize
-    @tabs = [
-      Tab.new('demandes', dashboard_show_path(id: 'demandes', **request.query_parameters)),
-      Tab.new('habilitations', dashboard_show_path(id: 'habilitations', **request.query_parameters)),
-    ]
+  def show
+    @facade = DashboardFacade.new(current_user, params[:search_query], subdomain_types: current_subdomain_types)
+    @tabs = build_tabs
 
     case params[:id]
     when 'demandes'
-      items = policy_scope(base_relation).includes(:applicant, :authorizations).not_archived.order(created_at: :desc).or(authorization_request_mentions_query)
-      @highlighted_categories = {
-        changes_requested: items.changes_requested,
-      }
-      @categories = {
-        pending: items.in_instructions,
-        draft: items.drafts,
-        refused: items.refused,
-      }
+      handle_demandes_tab
     when 'habilitations'
-      items = policy_scope(base_authorization_relation).includes(:applicant, :request).order(created_at: :desc).or(authorization_mentions_query)
-
-      @highlighted_categories = {}
-      @categories = {
-        active: items.where(state: :active),
-        revoked: items.where(state: :revoked),
-      }
+      handle_habilitations_tab
     else
       redirect_to(dashboard_show_path(id: 'demandes')) and return
     end
@@ -39,34 +23,34 @@ class DashboardController < AuthenticatedUserController
 
   private
 
-  def authorization_mentions_query
-    Authorization.where("EXISTS (
-        select 1
-        from each(authorizations.data) as kv
-        where kv.key like '%_email' and kv.value = ?
-      )", current_user.email)
+  def handle_demandes_tab
+    data = @facade.demandes_data(policy_scope(AuthorizationRequest))
+    @highlighted_categories = data[:highlighted_categories]
+    @categories = data[:categories]
+    @search_engine = data[:search_engine]
   end
 
-  def authorization_request_mentions_query
-    AuthorizationRequestsMentionsQuery.new(current_user).perform(authorization_requests_relation)
+  def handle_habilitations_tab
+    data = @facade.habilitations_data(policy_scope(Authorization))
+    @highlighted_categories = data[:highlighted_categories]
+    @categories = data[:categories]
+    @search_engine = data[:search_engine]
   end
 
-  def authorization_requests_relation
-    if registered_subdomain?
-      base_relation.where(type: registered_subdomain.authorization_request_types)
-    else
-      base_relation.all
-    end
+  def build_tabs
+    [
+      Tab.new('demandes', dashboard_show_path(id: 'demandes', **query_parameters)),
+      Tab.new('habilitations', dashboard_show_path(id: 'habilitations', **query_parameters)),
+    ]
   end
 
-  def base_relation
-    AuthorizationRequest
-      .joins('left join authorizations on authorizations.request_id = authorization_requests.id')
-      .select('authorization_requests.*, count(authorizations.id) as authorizations_count')
-      .group('authorization_requests.id')
+  def query_parameters
+    request.query_parameters
   end
 
-  def base_authorization_relation
-    Authorization.joins(request: :organization).where(authorization_requests: { organization: current_user.organizations })
+  def current_subdomain_types
+    return unless registered_subdomain?
+
+    registered_subdomain.authorization_request_types
   end
 end
