f602dbb6249d13553170fdaf89f2e1c5b8292d9b
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-07-23 09:42:22 +0200

    Add a non regression test to MalwareScan
      Add a before_destroy ensure_malware_scan_cleanup in case where PG violated forein key is raised
    
      Delete associate scan when an attachment is deleted

--- app/models/concerns/active_storage_attachment_extension.rb
+++ app/models/concerns/active_storage_attachment_extension.rb
@@ -5,5 +5,20 @@ module ActiveStorageAttachmentExtension
     has_one :malware_scan, dependent: :destroy
 
     delegate :safe?, :unsafe?, to: :malware_scan, allow_nil: true
+
+    before_destroy :ensure_malware_scan_cleanup
+  end
+
+  private
+
+  def ensure_malware_scan_cleanup
+    return if malware_scan.blank?
+
+    begin
+      malware_scan.destroy!
+    rescue ActiveRecord::RecordNotDestroyed, ActiveRecord::InvalidForeignKey => e
+      Rails.logger.warn "Failed to destroy malware_scan #{malware_scan.id} for attachment #{id}: #{e.message}"
+      malware_scan.delete
+    end
   end
 end
