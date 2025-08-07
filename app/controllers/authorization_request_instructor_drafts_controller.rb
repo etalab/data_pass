c5975ac8a9bfbbd5cec78791a1d571e63eee46b8
Delmaire Loïc <loic.delmaire@gmail.com>
2025-08-07 15:33:10 +0200

    Applicants can claim instructor draft

--- app/controllers/authorization_request_instructor_drafts_controller.rb
+++
@@ -1,5 +0,0 @@
-class AuthorizationRequestInstructorDraftsController < ApplicationController
-  def claim
-    @authorization_request_instructor_draft = AuthorizationRequestInstructorDraft.find_by!(public_id: params[:id])
-  end
-end
--- features/step_definitions/authorization_request_instructor_drafts_steps.rb
+++ features/step_definitions/authorization_request_instructor_drafts_steps.rb
@@ -8,3 +8,40 @@ Quand("j'ai une demande d'habilitation à partager pour {string} intitulée {str
     }
   )
 end
+
+Quand("on m'a invité à remplir une demande d'habilitation intitulée {string}") do |intitule|
+  user = User.find_by(email: @current_user_email)
+
+  create(
+    :authorization_request_instructor_draft,
+    applicant: user,
+    organization: create(:organization),
+    data: {
+      intitule:,
+    }
+  )
+end
+
+Quand("il y a un brouillon de demande d'habilitation initiée pour l'usager {string}") do |email|
+  user = User.find_by(email:) || create(:user, email:)
+
+  create(
+    :authorization_request_instructor_draft,
+    applicant: user,
+    organization: user.current_organization,
+    data: {
+      intitule: 'Projet de test',
+    }
+  )
+end
+
+Quand("je me rends sur cette invitation à remplir une demande d'habilitation") do
+  authorization_request_draft = AuthorizationRequestInstructorDraft.last
+
+  visit claim_authorization_request_instructor_draft_path(authorization_request_draft.public_id)
+end
+
+Quand("ce brouillon de demande d'habilitation a déjà été revendiquée") do
+  authorization_request_draft = AuthorizationRequestInstructorDraft.last
+  authorization_request_draft.update!(claimed: true)
+end
