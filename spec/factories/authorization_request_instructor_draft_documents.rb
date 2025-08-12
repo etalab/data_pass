4a352795e230627522ccbdb1f9b9a1936329653b
Delmaire Loïc <loic.delmaire@gmail.com>
2025-08-12 17:08:12 +0200

    Introduce AuthorizationRequestInstructorDraftDocument

---
+++ spec/factories/authorization_request_instructor_draft_documents.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :authorization_request_instructor_draft_document do
+    identifier { :cadre_juridique_document }
+    authorization_request_instructor_draft
+  end
+end
+
