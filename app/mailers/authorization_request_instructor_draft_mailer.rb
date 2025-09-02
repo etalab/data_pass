fb42bdb05eae9d49176e27ce4a65e382b8df2364
Delmaire Loïc <loic.delmaire@gmail.com>
2025-09-02 16:50:52 +0200

    Enhance AuthorizationRequestInstructorDraftMailer by notitying all reporters

--- app/mailers/authorization_request_instructor_draft_mailer.rb
+++ app/mailers/authorization_request_instructor_draft_mailer.rb
@@ -1,11 +1,17 @@
-class AuthorizationRequestInstructorDraftMailer < ApplicationMailer
+class AuthorizationRequestInstructorDraftMailer < AbstractInstructionMailer
   def invite_applicant
     @authorization_request_instructor_draft = params[:authorization_request_instructor_draft]
 
     mail(
       to: @authorization_request_instructor_draft.applicant.email,
-      cc: @authorization_request_instructor_draft.instructor.email,
+      bcc: instructors_or_reporters_to_notify.pluck(:email),
       subject: t('.subject', project_name: @authorization_request_instructor_draft.project_name)
     )
+  end
+
+  protected
+
+  def model_for_instructors_or_reporters
+    @authorization_request_instructor_draft
   end
 end
