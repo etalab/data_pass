274bc1da96776b76887762fc2c7a0f43c676130d
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-09-22 11:26:40 +0200

    Add non regression tests

--- spec/decorators/organization_decorator_spec.rb
+++ spec/decorators/organization_decorator_spec.rb
@@ -16,4 +16,50 @@ RSpec.describe OrganizationDecorator, type: :decorator do
       it { is_expected.to eq('Foreign address') }
     end
   end
+
+  describe '#code_naf_with_libelle' do
+    subject { organization.decorate.code_naf_with_libelle }
+
+    let(:organization) do
+      create(:organization,
+        legal_entity_registry: 'insee_sirene',
+        insee_payload: {
+          'etablissement' => {
+            'uniteLegale' => {
+              'activitePrincipaleUniteLegale' => code_naf
+            }
+          }
+        })
+    end
+
+    context 'when code_naf is nil' do
+      let(:code_naf) { nil }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when code_naf is blank' do
+      let(:code_naf) { '' }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when code_naf is 0000Z' do
+      let(:code_naf) { '0000Z' }
+
+      it { is_expected.to eq('0000Z - Activité inconnue') }
+    end
+
+    context 'when code_naf is valid' do
+      let(:code_naf) { '0112Z' }
+
+      it { is_expected.to eq('0112Z - Culture du riz') }
+    end
+
+    context 'when code_naf is invalid (not 0000Z)' do
+      let(:code_naf) { '9999X' }
+
+      it { is_expected.to eq('9999X - Activité inconnue') }
+    end
+  end
 end
