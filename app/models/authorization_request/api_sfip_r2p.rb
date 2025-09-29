3e82816cfe3381936723da05466a8f0306ad20a1
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-09-29 10:45:13 +0200

    Add 2 new modalities to Api_sfip_r2p
      - api_sfip_r2p_sandbox fr config modalities inherit from api_r2p_sandbox modalities values.
        add 2 new values / label for api_sfip_r2p forms

--- app/models/authorization_request/api_sfip_r2p.rb
+++ app/models/authorization_request/api_sfip_r2p.rb
@@ -9,7 +9,14 @@ class AuthorizationRequest::APISFiPR2P < AuthorizationRequest
   include AuthorizationExtensions::Volumetrie
   include DGFIPExtensions::ExtraContactsInfos
 
-  MODALITIES = %w[with_acces_etat_civil with_acces_spi with_acces_etat_civil_et_adresse with_acces_etat_civil_restitution_spi with_acces_etat_civil_complet_adresse with_acces_etat_civil_degrade_adresse].freeze
+  MODALITIES = %w[with_acces_etat_civil
+                  with_acces_spi
+                  with_acces_etat_civil_et_adresse
+                  with_acces_etat_civil_restitution_spi
+                  with_acces_etat_civil_complet_adresse
+                  with_acces_etat_civil_degrade_adresse
+                  with_acces_etat_civil_complet_restitution_complet_adresse_cp_coordonnées
+                  with_acces_etat_civil_degrade_adresse_restitution_complet_adresse_cp_coordonnées].freeze
 
   VOLUMETRIES = {
     '50 appels / minute': 50,
