f75b1cd0b7633b2f28af847c5ff212b884009847
Delmaire Loïc <loic.delmaire@gmail.com>
2025-10-08 12:41:52 +0200

    Instruction: bugfix FC tabs with multi stages authorizations
    Closes https://linear.app/pole-api/issue/DAT-1254

--- app/controllers/instruction/france_connected_authorizations_controller.rb
+++ app/controllers/instruction/france_connected_authorizations_controller.rb
@@ -2,7 +2,7 @@ class Instruction::FranceConnectedAuthorizationsController < Instruction::Abstra
   def index
     authorize [:instruction, @authorization_request], :show?
 
-    @france_connected_authorizations = @authorization_request.france_connected_authorizations
+    @france_connected_authorizations = @authorization_request.france_connected_authorizations.decorate
   end
 
   private
