5ae29c4205a77091e59c78f292f293651c4331cb
Delmaire Loïc <loic.delmaire@gmail.com>
2025-10-09 11:10:20 +0200

    Introduce manager roles for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,6 +44,19 @@ RSpec.describe User do
     it { is_expected.to contain_exactly(valid_reporter, valid_developer, valid_instructor, valid_instructor_with_multiple_authorization_type) }
   end
 
+  describe '.manager_for' do
+    subject { described_class.manager_for(authorization_request_type) }
+
+    let(:authorization_request_type) { 'api_entreprise' }
+
+    let!(:valid_manager) { create(:user, :manager, authorization_request_types: %i[api_entreprise]) }
+    let!(:valid_manager_with_multiple_authorization_type) { create(:user, :manager, authorization_request_types: %i[api_entreprise api_particulier]) }
+    let!(:invalid_manager) { create(:user, :manager, authorization_request_types: %i[api_particulier]) }
+    let!(:valid_instructor) { create(:user, :instructor, authorization_request_types: %i[api_entreprise]) }
+
+    it { is_expected.to contain_exactly(valid_manager, valid_manager_with_multiple_authorization_type) }
+  end
+
   describe '#reporter?' do
     subject { user.reporter?(authorization_request_type) }
 
@@ -126,6 +139,30 @@ RSpec.describe User do
         end
       end
     end
+
+    context 'when user is a manager' do
+      let(:user) { build(:user, :manager, authorization_request_types: %w[api_entreprise]) }
+
+      context 'without authorization_request_type' do
+        let(:authorization_request_type) { nil }
+
+        it { is_expected.to be_truthy }
+      end
+
+      context 'with authorization_request_type' do
+        context 'when authorization_request_type matches' do
+          let(:authorization_request_type) { 'api_entreprise' }
+
+          it { is_expected.to be_truthy }
+        end
+
+        context 'when authorization_request_type does not matche' do
+          let(:authorization_request_type) { 'api_particulier' }
+
+          it { is_expected.to be_falsey }
+        end
+      end
+    end
   end
 
   describe '#instructor?' do
@@ -149,6 +186,74 @@ RSpec.describe User do
 
     context 'when user is an instructor' do
       let(:user) { build(:user, :instructor, authorization_request_types: %w[api_entreprise]) }
+
+      context 'without authorization_request_type' do
+        let(:authorization_request_type) { nil }
+
+        it { is_expected.to be_truthy }
+      end
+
+      context 'with authorization_request_type' do
+        context 'when authorization_request_type matches' do
+          let(:authorization_request_type) { 'api_entreprise' }
+
+          it { is_expected.to be_truthy }
+        end
+
+        context 'when authorization_request_type does not matche' do
+          let(:authorization_request_type) { 'api_particulier' }
+
+          it { is_expected.to be_falsey }
+        end
+      end
+    end
+
+    context 'when user is a manager' do
+      let(:user) { build(:user, :manager, authorization_request_types: %w[api_entreprise]) }
+
+      context 'without authorization_request_type' do
+        let(:authorization_request_type) { nil }
+
+        it { is_expected.to be_truthy }
+      end
+
+      context 'with authorization_request_type' do
+        context 'when authorization_request_type matches' do
+          let(:authorization_request_type) { 'api_entreprise' }
+
+          it { is_expected.to be_truthy }
+        end
+
+        context 'when authorization_request_type does not matche' do
+          let(:authorization_request_type) { 'api_particulier' }
+
+          it { is_expected.to be_falsey }
+        end
+      end
+    end
+  end
+
+  describe '#manager?' do
+    subject { user.manager?(authorization_request_type) }
+
+    context 'when user is not a manager' do
+      let(:user) { build(:user) }
+
+      context 'without authorization_request_type' do
+        let(:authorization_request_type) { nil }
+
+        it { is_expected.to be_falsey }
+      end
+
+      context 'with authorization_request_type' do
+        let(:authorization_request_type) { 'api_entreprise' }
+
+        it { is_expected.to be_falsey }
+      end
+    end
+
+    context 'when user is a manager' do
+      let(:user) { build(:user, :manager, authorization_request_types: %w[api_entreprise]) }
 
       context 'without authorization_request_type' do
         let(:authorization_request_type) { nil }
