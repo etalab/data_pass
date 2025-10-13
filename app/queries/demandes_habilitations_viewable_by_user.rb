be6dbb0a5d94f2eae55ec67c9dbe79c664e29bdd
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2025-10-13 11:34:27 +0200

    feat: add query class for counting viewable items by user
    Extract reusable query logic to count authorization requests and
    authorizations that are viewable by a specific user. This supports
    both organization-scoped items and items where the user is mentioned.
    
    - Handles AuthorizationRequest and Authorization models
    - Filters out archived requests
    - Includes proper associations for each model type
    - Combines organization scope with mentions

---
+++ app/queries/demandes_habilitations_viewable_by_user.rb
@@ -0,0 +1,36 @@
+class DemandesHabilitationsViewableByUser
+  attr_reader :user, :scoped_relation, :model_class
+
+  def initialize(user, scoped_relation, model_class)
+    @user = user
+    @scoped_relation = scoped_relation
+    @model_class = model_class
+  end
+
+  delegate :count, to: :relation
+
+  private
+
+  def relation
+    scoped_items = scoped_relation.includes(includes_for_model)
+
+    scoped_items = scoped_items.not_archived if model_class.name == 'AuthorizationRequest'
+
+    mentions_items = AuthorizationAndRequestsMentionsQuery
+      .new(user)
+      .perform(model_class.all)
+
+    scoped_items.or(mentions_items)
+  end
+
+  def includes_for_model
+    case model_class.name
+    when 'Authorization'
+      %i[request applicant]
+    when 'AuthorizationRequest'
+      %i[applicant authorizations]
+    else
+      []
+    end
+  end
+end
