036c7dca7777d6e4b8888367bd0dbaeceea83348
Delmaire Loïc <loic.delmaire@gmail.com>
2025-10-28 18:39:25 +0100

    Create webhooks models

---
+++ spec/factories/webhooks.rb
@@ -0,0 +1,30 @@
+FactoryBot.define do
+  factory :webhook do
+    authorization_definition_id { 'api_entreprise' }
+    url { 'https://webhook.site/test' }
+    sequence(:secret) { |n| "secret_token_#{n}" }
+    events { %w[approve submit] }
+    enabled { false }
+    validated { false }
+    activated_at { nil }
+
+    trait :validated do
+      validated { true }
+      activated_at { Time.current }
+    end
+
+    trait :enabled do
+      enabled { true }
+    end
+
+    trait :active do
+      validated { true }
+      enabled { true }
+      activated_at { Time.current }
+    end
+
+    trait :with_all_events do
+      events { Webhook::VALID_EVENTS }
+    end
+  end
+end
