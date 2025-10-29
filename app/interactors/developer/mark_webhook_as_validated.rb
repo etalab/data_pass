81da9730018c152ead78db3d092adedc9c510a8f
Delmaire Loïc <loic.delmaire@gmail.com>
2025-10-29 19:06:11 +0100

    Introduce Developer::UpdateWebhook

--- app/interactors/developer/mark_webhook_as_validated.rb
+++ app/interactors/developer/mark_webhook_as_validated.rb
@@ -3,5 +3,6 @@ class Developer::MarkWebhookAsValidated < ApplicationInteractor
     return unless context.webhook_test[:success]
 
     context.webhook.mark_as_valid!
+    context.webhook.activate! if context.webhook_url_changed
   end
 end
