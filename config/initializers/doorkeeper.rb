4f9358e5d6ee79767f30db42f6e67fd92178f41e
Delmaire Loïc <loic.delmaire@gmail.com>
2025-10-31 12:59:38 +0100

    Developers API: Add polling for webhook calls

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -243,7 +243,7 @@ Doorkeeper.configure do
   # https://doorkeeper.gitbook.io/guides/ruby-on-rails/scopes
   #
   default_scopes  :public, :read_authorizations
-  optional_scopes :write_authorizations
+  optional_scopes :write_authorizations, :read_webhooks
 
   # Allows to restrict only certain scopes for grant_type.
   # By default, all the scopes will be available for all the grant types.
