3c46c4ff34cbd0e5ed6d400f3e775f1a08c4830a
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2025-11-12 14:20:52 +0100

    Add decorators

---
+++ spec/decorators/eligibility_rule_decorator_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe EligibilityRuleDecorator, type: :decorator do
+  let(:eligibility_rule) do
+    EligibilityRule.find_by(definition_id: 'api_particulier')
+  end
+
+  let(:decorator) { eligibility_rule.decorate }
+
+  describe '#options' do
+    it 'returns an array of decorated EligibilityOptions' do
+      expect(decorator.options).to be_an(Array)
+      expect(decorator.options).to all(be_a(EligibilityOptionDecorator))
+    end
+
+    it 'returns the correct number of options' do
+      expect(decorator.options.size).to eq(3)
+    end
+
+    it 'each option is decorated and functional' do
+      decorator.options.each do |option|
+        expect(option.id).to be_present
+        expect(option.outcome_id).to be_present
+        expect(option.label).to be_present
+      end
+    end
+  end
+
+  describe '#request_access_path' do
+    it 'returns the correct path for the API' do
+      expected_path = '/demandes/api_particulier/nouveau?eligibility_confirmed=true'
+      expect(decorator.request_access_path).to eq(expected_path)
+    end
+  end
+
+  describe 'delegation' do
+    it 'delegates unknown methods to the wrapped eligibility rule' do
+      expect(decorator.definition_id).to eq('api_particulier')
+      expect(decorator.id).to eq(eligibility_rule.id)
+    end
+  end
+end
--- spec/models/eligibility_rule_spec.rb
+++ spec/models/eligibility_rule_spec.rb
@@ -1,36 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe EligibilityRule do
-  describe '.find_by_definition_id' do
-    it 'returns the rule for a valid definition_id' do
-      rule = described_class.find_by(definition_id: 'api-particulier')
-
-      expect(rule).to be_present
-      expect(rule.definition_id).to eq('api-particulier')
-    end
-
-    it 'returns nil for an unknown definition_id' do
-      rule = described_class.find_by(definition_id: 'unknown-api')
-
-      expect(rule).to be_nil
-    end
-  end
-
   describe '#options' do
-    it 'returns an array of options' do
-      rule = described_class.find_by(definition_id: 'api-particulier')
+    it 'returns an array of EligibilityOption objects' do
+      rule = described_class.find_by(definition_id: 'api_particulier')
 
       expect(rule.options).to be_an(Array)
+      expect(rule.options).to all(be_a(EligibilityOption))
       expect(rule.options.size).to eq(3)
-    end
-
-    it 'options have the correct structure' do
-      rule = described_class.find_by(definition_id: 'api-particulier')
-      option = rule.options.first
-
-      expect(option).to have_key('type')
-      expect(option).to have_key('eligible')
-      expect(option).to have_key('body')
     end
   end
 end
