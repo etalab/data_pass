ecf2caa5b91eb6c3c91206dc4ef35b0923aa558f
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-11-13 10:54:40 +0100

    udpate contacts

--- app/models/authorization_request/produits_dinum.rb
+++ app/models/authorization_request/produits_dinum.rb
@@ -1,15 +1,16 @@
 class AuthorizationRequest::ProduitsDinum < AuthorizationRequest
   include AuthorizationExtensions::CadreJuridique
-  include AuthorizationExtensions::GDPRContacts
-
-  add_documents :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
-
-  add_attributes :date_prevue_mise_en_production,
-    :volumetrie_approximative
 
   add_scopes(validation: {
     presence: true, if: -> { need_complete_validation?(:scopes) }
   })
 
-  contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+  %i[
+    contact_metier
+    contact_technique
+    delegue_protection_donnees
+    responsable_administration
+  ].each do |contact_kind|
+    contact contact_kind, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+  end
 end
