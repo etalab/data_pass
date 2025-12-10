6de429f5089ef8f832fc12c9043ca21b820be8ca
Delmaire Loïc <loic.delmaire@gmail.com>
2025-12-10 17:20:46 +0100

    Refactor habilitation alerts within a component
    Dummy refactor here

---
+++ spec/components/previews/habilitation/user_alerts_component_preview.rb
@@ -0,0 +1,48 @@
+# @label Alertes utilisateur sur une habilitation
+class Habilitation::UserAlertsComponentPreview < ApplicationPreview
+  # @label 1. Mention en tant que contact
+  def contact_mention
+    contact_user = User.find_by!(email: 'user@yopmail.com')
+    authorization_request = AuthorizationRequest
+      .where(state: 'validated')
+      .where("EXISTS (
+      select 1
+      from each(authorization_requests.data) as kv
+      where kv.key like '%_email' and lower(kv.value) = ?
+    )", contact_user.email)
+      .first!
+
+    render Habilitation::UserAlertsComponent.new(authorization: authorization_request.latest_authorization, current_user: contact_user)
+  end
+
+  # @label 2. Mise à jour de la demande en cours
+  def update_in_progress
+    authorization_request = AuthorizationRequest.where(state: 'draft').where.not(last_validated_at: nil).first!
+    authorization = authorization_request.latest_authorization
+
+    render Habilitation::UserAlertsComponent.new(authorization:, current_user: authorization_request.applicant)
+  end
+
+  # @label 3. Ancienne version de l'habilitation
+  def old_version
+    authorization_request = AuthorizationRequest.joins(:authorizations)
+      .where(type: 'AuthorizationRequest::APIEntreprise')
+      .group('authorization_requests.id')
+      .having('COUNT(authorizations.id) >= 2')
+      .first!
+
+    old_authorization = authorization_request.authorizations.reject(&:latest?).first
+
+    render Habilitation::UserAlertsComponent.new(authorization: old_authorization, current_user: authorization_request.applicant)
+  end
+
+  # @label 4. Bandeau accès disponible
+  def access_callout
+    authorization_request = AuthorizationRequest
+      .where(type: 'AuthorizationRequest::APIEntreprise')
+      .where.not(external_provider_id: nil)
+      .find { |ar| ar if ar.access_link.present? && ar.validated? }
+
+    render Habilitation::UserAlertsComponent.new(authorization: authorization_request.latest_authorization, current_user: authorization_request.applicant)
+  end
+end
