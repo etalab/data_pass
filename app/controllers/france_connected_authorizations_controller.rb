77d36201460f9154972356238ca0aab3c645d3dc
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2026-01-09 18:17:50 +0100

    Extract AuthorizationOrRequestContext concern
    Reduce duplication by extracting shared code from controllers that handle
    both authorization_request and authorization contexts:
    - extract_authorization_or_request method
    - layout_name method
    - helper and decorates_assigned declarations

--- app/controllers/france_connected_authorizations_controller.rb
+++ app/controllers/france_connected_authorizations_controller.rb
@@ -1,29 +1,9 @@
 class FranceConnectedAuthorizationsController < AuthenticatedUserController
-  helper DemandesHabilitations::CommonHelper
-
-  before_action :extract_authorization_or_request
-  decorates_assigned :authorization_request, :authorization
+  include AuthorizationOrRequestContext
 
   def index
     authorize @authorization_request, :france_connected_authorizations?
 
     @france_connected_authorizations = @authorization_request.france_connected_authorizations.decorate
-  end
-
-  private
-
-  def extract_authorization_or_request
-    if params[:authorization_id].present?
-      @authorization = Authorization.friendly.find(params[:authorization_id])
-      @authorization_request = @authorization.request
-    else
-      @authorization_request = AuthorizationRequest.find(params[:authorization_request_id])
-    end
-  end
-
-  def layout_name
-    return 'authorization_with_tabs' if @authorization.present?
-
-    'authorization_request_with_tabs'
   end
 end
