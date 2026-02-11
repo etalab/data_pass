7c5a3861ba71395bd9dae64be1068cb4b687bfc5
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2026-02-11 11:13:13 +0100

    nouvelle API GUNenv

---
+++ app/models/authorization_request/api_gunenv.rb
@@ -0,0 +1,17 @@
+class AuthorizationRequest::APIGunenv < AuthorizationRequest
+  include AuthorizationExtensions::BasicInfos
+  include AuthorizationExtensions::CadreJuridique
+  include AuthorizationExtensions::PersonalData
+  include AuthorizationExtensions::GDPRContacts
+
+  add_documents :maquette_projet, content_type: ['application/pdf'], size: { less_than: 10.megabytes }
+
+  add_attributes :date_prevue_mise_en_production,
+    :volumetrie_approximative
+
+  add_scopes(validation: {
+    presence: true, if: -> { need_complete_validation?(:scopes) }
+  })
+
+  contact :contact_technique, validation_condition: ->(record) { record.need_complete_validation?(:contacts) }
+end
