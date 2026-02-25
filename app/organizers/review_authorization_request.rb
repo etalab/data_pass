e20ad7709dced0abfc4af93611be55eac4234e3f
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2026-02-25 14:29:28 +0100

    Valider une réouverture APIP compatible APIPFC avec la modalité FC génére une habilitation FC

--- app/organizers/review_authorization_request.rb
+++ app/organizers/review_authorization_request.rb
@@ -2,10 +2,12 @@ class ReviewAuthorizationRequest < ApplicationOrganizer
   before do
     context.authorization_request_params ||= ActionController::Parameters.new
     context.save_context = :review
+    context.skip_validation = true
     context.fail! unless context.authorization_request.filling?
   end
 
-  organize AssignParamsToAuthorizationRequest
+  organize AssignParamsToAuthorizationRequest,
+    AssignFranceConnectDefaultsOnReopening
 
   after do
     context.authorization_request.save(context: context.save_context) ||
