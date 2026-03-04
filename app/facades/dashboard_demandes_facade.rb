2ea951088f9cb4c5b30610c171b91c28c49e5be3
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2026-03-04 17:57:51 +0100

    Fix dashboard n+1

--- app/facades/dashboard_demandes_facade.rb
+++ app/facades/dashboard_demandes_facade.rb
@@ -1,16 +1,16 @@
 class DashboardDemandesFacade < AbstractDashboardFacade
   def data
     builder = search_builder
-    demandes = builder.build_relation(scoped_relation)
+    grouped = builder.build_relation(scoped_relation.where(state: displayed_states)).to_a.group_by(&:state)
 
     {
       highlighted_categories: {
-        changes_requested: demandes.changes_requested,
+        changes_requested: grouped.fetch('changes_requested', []),
       },
       categories: {
-        pending: demandes.in_instructions,
-        draft: demandes.drafts,
-        refused: demandes.refused,
+        pending: grouped.fetch('submitted', []),
+        draft: grouped.fetch('draft', []),
+        refused: grouped.fetch('refused', []),
       },
       search_engine: builder.search_engine
     }
