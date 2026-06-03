0d95d3095bd27f27ec619f4494925b52856c4938
Delmaire Loïc <loic.delmaire@gmail.com>
2026-06-03 17:10:02 +0200

    Améliore les prévisualisation des emails FranceConnect

--- spec/mailers/previews/france_connect_mailer_preview.rb
+++ spec/mailers/previews/france_connect_mailer_preview.rb
@@ -3,9 +3,17 @@ class FranceConnectMailerPreview < ActionMailer::Preview
     FranceConnectMailer.with(authorization_request:).new_scopes
   end
 
+  def new_scopes_reopening
+    FranceConnectMailer.with(authorization_request: reopened_authorization_request).new_scopes
+  end
+
   private
 
   def authorization_request
     AuthorizationRequest::APIImpotParticulierSandbox.where(state: :validated).last
+  end
+
+  def reopened_authorization_request
+    AuthorizationRequest::APIImpotParticulierSandbox.where.not(reopened_at: nil).where(state: :submitted).last
   end
 end
