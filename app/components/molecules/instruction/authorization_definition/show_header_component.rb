328c6d81d4f690b7224cce2ba1886a67cd4ad326
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2026-06-23 10:28:19 +0200

    Extract wide_header instances into dedicated components
    Create ShowHeaderComponent for the authorization definition show page,
    replacing inline WideHeader usage with a dedicated component and preview.

---
+++ app/components/molecules/instruction/authorization_definition/show_header_component.rb
@@ -0,0 +1,29 @@
+class Molecules::Instruction::AuthorizationDefinition::ShowHeaderComponent < ApplicationComponent
+  def initialize(authorization_definition:, validated_count:, submitted_count:, can_initiate_request: false)
+    @authorization_definition = authorization_definition
+    @validated_count = validated_count
+    @submitted_count = submitted_count
+    @can_initiate_request = can_initiate_request
+  end
+
+  private
+
+  attr_reader :authorization_definition, :validated_count, :submitted_count, :can_initiate_request
+
+  def back_link
+    {
+      path: helpers.instruction_authorization_definitions_path,
+      text: I18n.t('page_titles.instruction_definitions')
+    }
+  end
+
+  def initiate_request_path
+    helpers.start_instruction_instructor_draft_requests_path(
+      form_uid: authorization_definition.default_form.uid
+    )
+  end
+
+  def request_url
+    helpers.new_authorization_request_url(authorization_definition.id)
+  end
+end
