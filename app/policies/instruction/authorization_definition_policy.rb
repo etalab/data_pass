0394f7e6c7362f30bf5c982ba1e11dfd7331c636
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2026-06-25 10:11:17 +0200

    feature flag : display menu only for admins

--- app/policies/instruction/authorization_definition_policy.rb
+++ app/policies/instruction/authorization_definition_policy.rb
@@ -1,5 +1,9 @@
 class Instruction::AuthorizationDefinitionPolicy < ApplicationPolicy
   def index?
-    user.reporter?
+    user.reporter? && feature_flag_show_definitions?
+  end
+
+  def feature_flag_show_definitions?
+    user.admin? || Rails.env.test?
   end
 end
