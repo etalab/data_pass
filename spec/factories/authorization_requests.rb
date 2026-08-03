c62988636a46e67d67ca211e0218d42437551921
Delmaire Loïc <loic.delmaire@gmail.com>
2026-08-03 14:26:27 +0200

    Référence l’éditeur optiCreche pour la tarification EAJE
    Themisto Conseil (SIRET 81194509600022) édite optiCreche, solution de
    gestion des établissements d’accueil du jeune enfant. Ajoute l’éditeur au
    catalogue et son formulaire pré-rempli pour le cas d’usage tarification
    EAJE : api-particulier-opticreche-petite-enfance
    
    Réf. https://linear.app/pole-api/issue/API-7123
    Closes https://linear.app/pole-api/issue/API-7124

--- spec/factories/authorization_requests.rb
+++ spec/factories/authorization_requests.rb
@@ -521,6 +521,7 @@ FactoryBot.define do
       api-particulier-familea-petite-enfance
       api-particulier-hoptis
       api-particulier-hoptis-petite-enfance
+      api-particulier-opticreche-petite-enfance
       api-particulier-andyvie
       api-particulier-polycea
     ].each do |form_uid|
