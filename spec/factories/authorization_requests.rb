f09c861f4af75d5f2a418913036f3c9cadfd9160
Delmaire Loïc <loic.delmaire@gmail.com>
2026-08-03 14:27:04 +0200

    Référence l’éditeur CapCreche pour la tarification EAJE
    ICAP (SIRET 48824909500032) édite CapCreche, solution de gestion des
    établissements d’accueil du jeune enfant. Ajoute l’éditeur au catalogue et
    son formulaire pré-rempli pour le cas d’usage tarification EAJE :
    api-particulier-capcreche-petite-enfance
    
    Réf. https://linear.app/pole-api/issue/API-7123
    Closes https://linear.app/pole-api/issue/API-7155

--- spec/factories/authorization_requests.rb
+++ spec/factories/authorization_requests.rb
@@ -522,6 +522,7 @@ FactoryBot.define do
       api-particulier-hoptis
       api-particulier-hoptis-petite-enfance
       api-particulier-opticreche-petite-enfance
+      api-particulier-capcreche-petite-enfance
       api-particulier-andyvie
       api-particulier-polycea
     ].each do |form_uid|
