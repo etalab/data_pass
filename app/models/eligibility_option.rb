3b23ad0a73a2ab6ba920e42de8cd4d149a40ab92
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2026-09-25 15:04:25 +0200

    Filtre les formulaires EAJE après le choix EAJE du préfiltrage
    Réutilise le filtre ?use_case= de la page API Particulier, jamais alimenté jusqu’ici.
    La tuile « Demande libre » reste visible, la liste des éditeurs n’est pas filtrée.

--- app/models/eligibility_option.rb
+++ app/models/eligibility_option.rb
@@ -1,12 +1,13 @@
 class EligibilityOption
   include Draper::Decoratable
 
-  attr_reader :type, :eligible, :body, :cta
+  attr_reader :type, :eligible, :body, :cta, :use_case
 
   def initialize(attributes)
     @type = attributes['type']
     @eligible = attributes['eligible']
     @body = attributes['body']
     @cta = attributes['cta']
+    @use_case = attributes['use_case']
   end
 end
